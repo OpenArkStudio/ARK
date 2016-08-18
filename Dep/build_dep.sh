d2fe75c6d2ddceab9b7c5649968601d0f88c6eb2
NickYang <elite_yang@163.com>
2016-08-18 15:23:10 +0800

    first commit for Windows, Linux is trying
    Former-commit-id: f0af87537274f97b0a8db6ce612e959cdec05dbf
    Former-commit-id: c133bca23870519e7d7b2c77fc2d8630263bdb4d

---
+++ Dep/build_dep.sh
@@ -0,0 +1,65 @@
+echo Building dependencies...
+
+mkdir lib/
+mkdir lib/Debug/
+mkdir lib/Release/
+
+# extract mysql libs
+
+
+# extract libevent libs
+echo Extract libevent libs
+tar -xzvf libevent-2.0.22-stable.tar.gz
+if [ -d ./libevent ]; then
+ rm -rf ./libevent
+fi
+
+mv libevent-2.0.22-stable libevent
+
+# compiling libevent
+cd libevent
+chmod -R 755 *
+./configure CPPFLAGS=-fPIC --disable-shared --disable-openssl
+make
+
+cp -R -f .libs/*.a ../lib/Debug/
+cp -R -f .libs/*.a ../lib/
+cp -R -f .libs/*.a ../lib/Release/
+cd ../
+
+# compiling protobuf
+cd protobuf
+chmod -R 755 *
+./configure CXXFLAGS=-fPIC
+make
+make check
+
+if [ ! -d ../../_Out/Comm/ ]; then
+ mkdir ../../_Out/Comm/
+fi
+if [ ! -d ../../_Out/Comm/Debug/ ]; then
+ mkdir ../../_Out/Comm/Debug/
+fi
+if [ ! -d ../../_Out/Comm/Release/ ]; then
+ mkdir ../../_Out/Comm/Release/
+fi
+
+cp -r -f ./src/.libs/*.so ../lib/Debug/
+cp -r -f ./src/.libs/*.so.* ../lib/Debug/
+cp -r -f ./src/.libs/*.so ../lib/Release/
+cp -r -f ./src/.libs/*.so.* ../lib/Release/
+cp -r -f ./src/.libs/*.so ../../_Out/Comm/Debug/
+cp -r -f ./src/.libs/*.so.* ../../_Out/Comm/Debug/
+cp -r -f ./src/.libs/*.so ../../_Out/Comm/Release/
+cp -r -f ./src/.libs/*.so.* ../../_Out/Comm/Release/
+
+cp -r -f ./src/.libs/*.so ../lib/
+cp -r -f ./src/.libs/*.so.* ../lib/
+cd ../
+
+
+# TODO: other libs
+
+# back to main dir
+pwd
+
