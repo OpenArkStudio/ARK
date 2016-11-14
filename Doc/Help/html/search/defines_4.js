4fce0ed90cca8fc223ae757fce821f443d554f23
flyicegood <flyicegood@163.com>
2016-11-14 21:06:15 +0800

    modify dox
    Former-commit-id: 413d7b23a96f56eb651afb3ca7948a3379e2d217 [formerly 327ccc4fa49af692f09e5959e115fbcf2c5a4535] [formerly be9f4972e83ad4576df2192cd53e11175b596a0b [formerly eaadcce72297c1e08ebc38c22ca865d538dec685]]
    Former-commit-id: a893468a33abd53768d33336843447589ffe6654 [formerly aca2ae194d7e88321f7f47cce27c73cb962b5094]
    Former-commit-id: ea7eb265fabe72a49deaca549ee63a296eef32c6
    Former-commit-id: c9f48bf59948cf0dcbdb3f9372f3adb1cee797b5

---
+++ Doc/Help/html/search/defines_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['find_5fmodule',['FIND_MODULE',['../_n_f_i_plugin_manager_8h.html#a1fb57a186649c068d08d1f01e2ecc575',1,'NFIPluginManager.h']]],
+  ['forceinline',['FORCEINLINE',['../_n_f_platform_8h.html#a724fb9f82013c782db5c3c12ea36aac8',1,'NFPlatform.h']]]
+];
