0c39ed8ebdddee3a0c1aafa76f52f07b0b66d276
flyicegood <flyicegood@163.com>
2016-11-14 21:06:15 +0800

    modify dox
    Former-commit-id: d55d79b5024aa823a93dc1c9a976515fc9e0d4d6 [formerly 8f6582e732ed6827075641adbbfd26989e862744] [formerly cf52e585d1d61008ca641572339d0400488c5c83 [formerly c25531262278b15da96c11d1c5be41cdccb5c35a]]
    Former-commit-id: 6f61bd755e8d270eb73ea901d6daa17f8be5aa21 [formerly 44abf23e1a4be5821c6b469fc6bae3b5c7858de1]
    Former-commit-id: 8082aeec2fe83609cb9c20201a46a9e8d7d09e5e
    Former-commit-id: 8797063485966fddecc758b42baf2fe2ce885984

---
+++ Doc/Help/html/search/defines_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['get_5fclass_5fname',['GET_CLASS_NAME',['../_n_f_i_plugin_8h.html#a887839265977b79104095ff8b7d7ab4a',1,'NFIPlugin.h']]]
+];
---
+++ Doc/Help/html/search/typedefs_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['heart_5fbeat_5ffunctor',['HEART_BEAT_FUNCTOR',['../_n_f_define_8h.html#aabd4fbf629d1d72351bcfaf824720a7d',1,'NFDefine.h']]],
+  ['heart_5fbeat_5ffunctor_5fptr',['HEART_BEAT_FUNCTOR_PTR',['../_n_f_define_8h.html#af05fc8cc77fb7df651e720c06ff2dc63',1,'NFDefine.h']]],
+  ['hinstance',['hInstance',['../_n_f_c_dyn_lib_8h.html#a46289065c8e4a79f640335241fc5e2ce',1,'NFCDynLib.h']]]
+];
