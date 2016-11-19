4ab0e30c3273037ee03167cbe61f99659b60196d
NickYang <elite_yang@163.com>
2016-11-19 16:01:08 +0800

    fix doc hyperlinks
    Former-commit-id: c152c7700238bfbebbccef534094f5e3c222433b
    Former-commit-id: 90a70f0a153e3937c77821a3fc7029fb294c15c8

--- Doc/Help/html/search/all_16.js
+++ Doc/Help/html/search/all_16.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['validcheck',['ValidCheck',['../class_n_f_c_record.html#a41ef028c68269cea9af585ff21969517',1,'NFCRecord']]],
   ['validcol',['ValidCol',['../class_n_f_c_record.html#a94d2329089a455eef490e2a2b98f05a3',1,'NFCRecord']]],
   ['validindex',['ValidIndex',['../class_n_f_c_data_list.html#a9e169677dfa5837f48adbcd09110df72',1,'NFCDataList']]],
   ['validpos',['ValidPos',['../class_n_f_c_record.html#a2997225c9557065e60e4703829178193',1,'NFCRecord']]],
