edeb97e485dd7be5f29b5c3009b6f6733be134fd
NickYang <elite_yang@163.com>
2017-02-28 18:25:20 +0800

    fix the console logo

--- Frame/NFComm/NFPluginLoader/NFPluginLoader.cpp
+++ Frame/NFComm/NFPluginLoader/NFPluginLoader.cpp
@@ -126,8 +126,8 @@ void PrintfLogo()
     std::cout << "\n" << std::endl;
     std::cout << "¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï" << std::endl;
     std::cout << "¡ï                                            ¡ï" << std::endl;
-    std::cout << "¡ï                 NoahFrame                  ¡ï" << std::endl;
-    std::cout << "¡ï   Copyright (c) 2011-2016  NFrame Studio   ¡ï" << std::endl;
+    std::cout << "¡ï                 ArkGame                    ¡ï" << std::endl;
+    std::cout << "¡ï   Copyright (c) 2011-2017  ArkGame Studio  ¡ï" << std::endl;
     std::cout << "¡ï             All rights reserved.           ¡ï" << std::endl;
     std::cout << "¡ï                                            ¡ï" << std::endl;
     std::cout << "¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï¡ï" << std::endl;
