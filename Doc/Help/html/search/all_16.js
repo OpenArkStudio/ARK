99d015ee2113c765bcca2bace2a6177c2417ada2
NickYang <elite_yang@163.com>
2017-03-02 16:50:07 +0800

    Update document
    Former-commit-id: cd20ae485f073d9bfd11a78de959b454c1152be7 [formerly 399eb69bf5fc1c48e7648d1e19cc05bb05fe676e] [formerly 7993e826eae4e2ea7bc2d6d64a98ab782fe808a0 [formerly 8d2900d7bf5ea56a796fdca527d18af9d4f4bc4b]]
    Former-commit-id: 315880346acaf00357db8b08a04c040e2c36a428 [formerly 2eee4b94cec1063239c05624fc7ac47a261dc2dc]
    Former-commit-id: 95698da79b887fbd42ba8aa46f492fd4da2028b0
    Former-commit-id: c676c1414fa035d396e85f77b23327f0d059ec6d

--- Doc/Help/html/search/all_16.js
+++ Doc/Help/html/search/all_16.js
@@ -5,7 +5,8 @@ var searchData=
   ['validindex',['ValidIndex',['../class_n_f_c_data_list.html#a9e169677dfa5837f48adbcd09110df72',1,'NFCDataList']]],
   ['validpos',['ValidPos',['../class_n_f_c_record.html#a2997225c9557065e60e4703829178193',1,'NFCRecord']]],
   ['validrow',['ValidRow',['../class_n_f_c_record.html#a1f456da47641a42a236954effbd95f01',1,'NFCRecord']]],
-  ['variantdata',['variantData',['../struct_n_f_i_data_list_1_1_t_data.html#ab19d96f0d7fa4bd828aae6b7390bf66a',1,'NFIDataList::TData']]],
+  ['variant_5ftype',['variant_type',['../struct_n_f_i_data_list_1_1_t_data.html#a1c5c25f1be80f2768983ace24ed7a759',1,'NFIDataList::TData']]],
+  ['variantdata',['variantData',['../struct_n_f_i_data_list_1_1_t_data.html#a67452dfe15470e58ad884e86e83027da',1,'NFIDataList::TData']]],
   ['vector3_2ehpp',['Vector3.hpp',['../_vector3_8hpp.html',1,'']]],
   ['verifyconnectdata',['VerifyConnectData',['../class_n_f_i_proxy_server_to_world_module.html#a05b4acafce1d32853b0c079c5dd53c55',1,'NFIProxyServerToWorldModule::VerifyConnectData()'],['../class_n_f_c_proxy_server_to_world_module.html#aa100690d965b17fd402ba01cca2b1b9f',1,'NFCProxyServerToWorldModule::VerifyConnectData()']]]
 ];
--- Doc/Help/html/search/classes_6.js
+++ Doc/Help/html/search/classes_6.js
@@ -1,8 +1,5 @@
 var searchData=
 [
-  ['sceneseedresource',['SceneSeedResource',['../struct_n_f_c_scene_process_module_1_1_scene_seed_resource.html',1,'NFCSceneProcessModule']]],
-  ['searchguildobject',['SearchGuildObject',['../struct_n_f_i_world_guild_data_module_1_1_search_guild_object.html',1,'NFIWorldGuildDataModule']]],
-  ['sendmsginfo',['SendmsgInfo',['../struct_n_f_c_mul_net_1_1_sendmsg_info.html',1,'NFCMulNet']]],
-  ['serverdata',['ServerData',['../struct_server_data.html',1,'']]],
-  ['sessiondata',['SessionData',['../struct_session_data.html',1,'']]]
+  ['recivemsginfo',['RecivemsgInfo',['../struct_n_f_c_mul_net_1_1_recivemsg_info.html',1,'NFCMulNet']]],
+  ['record_5fevent_5fdata',['RECORD_EVENT_DATA',['../struct_r_e_c_o_r_d___e_v_e_n_t___d_a_t_a.html',1,'']]]
 ];
