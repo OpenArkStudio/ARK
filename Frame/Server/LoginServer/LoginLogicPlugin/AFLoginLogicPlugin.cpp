c8413af2713a1f57041ba379ac1653f4f0d565dd
NickYang <362148418@qq.com>
2017-07-04 17:13:09 +0800

    Restructuring directories
    Former-commit-id: 7957ed9381d23f00b9073cff5c1d8ae21239a47b
    Former-commit-id: 11dc0311a9eba76220273f463248d7737e7816e4

---
+++ Frame/Server/LoginServer/LoginLogicPlugin/AFLoginLogicPlugin.cpp
@@ -0,0 +1,53 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFLoginLogicPlugin.cpp
+//    @Author           :    Ark Game Tech
+//    @Date             :    2012-07-14 08:51
+//    @Module           :    LoginLogicPluginModule
+//
+// -------------------------------------------------------------------------
+
+
+#include "NFLoginLogicPlugin.h"
+#include "NFCLoginLogicModule.h"
+
+//
+#ifdef NF_DYNAMIC_PLUGIN
+
+NF_EXPORT void DllStartPlugin(NFIPluginManager* pm)
+{
+#if NF_PLATFORM == NF_PLATFORM_WIN
+    SetConsoleTitle("NFLoginServer");
+#endif
+    CREATE_PLUGIN(pm, NFLoginLogicPlugin)
+
+};
+
+NF_EXPORT void DllStopPlugin(NFIPluginManager* pm)
+{
+    DESTROY_PLUGIN(pm, NFLoginLogicPlugin)
+};
+
+#endif
+//////////////////////////////////////////////////////////////////////////
+
+const int NFLoginLogicPlugin::GetPluginVersion()
+{
+    return 0;
+}
+
+const std::string NFLoginLogicPlugin::GetPluginName()
+{
+    return GET_CLASS_NAME(NFLoginLogicPlugin);
+}
+
+void NFLoginLogicPlugin::Install()
+{
+
+    REGISTER_MODULE(pPluginManager, NFILoginLogicModule, NFCLoginLogicModule)
+
+}
+
+void NFLoginLogicPlugin::Uninstall()
+{
+    UNREGISTER_MODULE(pPluginManager, NFILoginLogicModule, NFCLoginLogicModule)
+}
