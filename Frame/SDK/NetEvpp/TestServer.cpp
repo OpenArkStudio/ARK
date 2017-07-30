c2c83804abfe8d0afe87874c816497b079737a80
flyicegood <flyicegood@163.com>
2017-07-30 20:52:25 +0800

    modify net
    Former-commit-id: 74688e944c04fab05457bf273e21314d697c498e

--- Frame/SDK/NetEvpp/TestServer.cpp
+++ Frame/SDK/NetEvpp/TestServer.cpp
@@ -5,11 +5,11 @@
 #include <processthreadsapi.h>
 #include "SDK/Core/AFTime.h"
 #ifdef NF_DEBUG_MODE
-#pragma comment(lib,"NFNet_d.lib")
-#pragma comment(lib,"AFCore_d.lib")
+#pragma comment(lib,"AFNetEvpp_d.lib")
+#pragma comment(lib,"Core_d.lib")
 #else
-#pragma comment(lib,"NFNet.lib")
-#pragma comment(lib,"AFCore.lib")
+#pragma comment(lib,"AFNetEvpp.lib")
+#pragma comment(lib,"Core.lib")
 #endif
 
 class TestServerClass
@@ -19,7 +19,7 @@ public:
     {
         //pNet = new AFCNet(this, &TestServerClass::ReciveHandler, &TestServerClass::EventHandler);
         pNet = new AFCNetServer(this, &TestServerClass::ReciveHandler, &TestServerClass::EventHandler);
-        pNet->Initialization(10000, 8088, 2, 4);
+        pNet->Initialization(10000, "192.168.1.143:8088", 2, 1);
         nSendMsgCount = 0;
         nReciveMsgCount = 0;
         nStartTime = AFTime::GetUTCTime();
@@ -28,20 +28,20 @@ public:
         nLasterReciveCount = 0;
     }
 
-    void ReciveHandler(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID)
+    void ReciveHandler(const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID)
     {
         std::string str;
         str.assign(msg, nLen);
 
         nReciveMsgCount++;
         nSendMsgCount++;
-        pNet->SendMsgWithOutHead(nMsgID, msg, nLen, nSockIndex);
+        pNet->SendMsgWithOutHead(nMsgID, msg, nLen, xClientID);
         //std::cout << " nSendMsgCount: " << nSendMsgCount << "nReciveMsgCount" << nReciveMsgCount << " fd: " << nSockIndex << " msg_id: " << nMsgID /*<<  " data: " << str*/ << " thread_id: " << GetCurrentThreadId() << std::endl;
     }
 
-    void EventHandler(const int nSockIndex, const NF_NET_EVENT e, const AFGUID& xClientID, const int nServerID)
+    void EventHandler(const NetEventType e, const AFGUID& xClientID, const int nServerID)
     {
-        std::cout << " fd: " << nSockIndex << " event_id: " << e << " thread_id: " << std::this_thread::get_id() << std::endl;
+        std::cout << e << " event_id: " << e << " thread_id: " << std::this_thread::get_id() << std::endl;
     }
 
     void Execute()
@@ -85,12 +85,20 @@ int main(int argc, char** argv)
 {
     TestServerClass x;
 
+    int64_t nNowMS = 0;
+    int64_t nFrameTime = 0;
+    int64_t nPreFrameMS = 0;
     while(1)
     {
+        /*       nNowMS = AFTime::GetNowMillisecond();
+               nFrameTime = nNowMS - nPreFrameMS;
+               nPreFrameMS = nNowMS;
+               if(nFrameTime < 10 && nFrameTime >= 0)
+               {
+                   int64_t nNeeSleepMS = 10 - (nNowMS - nPreFrameMS);
+                   std::this_thread::sleep_for(std::chrono::milliseconds(nNeeSleepMS));
+               }*/
         x.Execute();
-
-        //handle other logic
-        NFSLEEP(1);
     }
 
     return 0;
