8289cd979a3fefd2f6db826175cb3df460f6a0fb
flyicegood <flyicegood@163.com>
2017-08-09 12:31:48 +0800

    fix build script
    Former-commit-id: 61778c92b9b7462393a9ee02fd32f55045c752a0
    Former-commit-id: f021d76940c8db5c7fa0af6050f95a9643ff36ab
    Former-commit-id: 1bcdb0e165ef297e01eb3fecce830cba2a43d281

--- Frame/Server/LoginServer/LoginNet_ServerPlugin/dllmain.cpp
+++ Frame/Server/LoginServer/LoginNet_ServerPlugin/dllmain.cpp
@@ -8,7 +8,7 @@
 #pragma comment( lib, "Core_d.lib" )
 #pragma comment( lib, "Proto_d.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
-#pragma comment( lib, "libevent_core.lib" )
+#pragma comment( lib, "event_core.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "AFNetEvpp_d.a" )
 #pragma comment( lib, "Core_d.a" )
@@ -24,7 +24,7 @@
 #pragma comment( lib, "Core.lib" )
 #pragma comment( lib, "Proto.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
-#pragma comment( lib, "libevent_core.lib" )
+#pragma comment( lib, "event_core.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "AFNetEvpp.a" )
 #pragma comment( lib, "Core.a" )
