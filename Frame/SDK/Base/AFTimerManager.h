209e6ce566fd3fbc8480ff7c52a49ed555dc8856
NickYang <362148418@qq.com>
2017-08-11 15:56:33 +0800

    add Noncopyable and TimerManager
    Former-commit-id: f1afc09729d15a0accc19c882de91f0bfb2e633f [formerly 25241fbe4eea018289ac56003b5afc4bf31f4071] [formerly 3b009668d2a7c96a305fc4d377b490dde942cc12] [formerly eb10c46d1a7eba87353ad6bf4a172705162bc156 [formerly 8a9e87da5a3b04572814f0388c088034e8c9377e]] [formerly 48b174d7ebab02aa2d793ff27f72bf4509eae0cd [formerly e6e9b734da3e6d125cb9a4f7d51061c53dd0c748] [formerly 7b8166282ac2c01917338ae8ef36170b4ff7f08d [formerly 33fa2d3cbcf50eb5b79ddd4a6fb7865b292e2156]]] [formerly 2cb47713ebf7ec02ee43dd4e382f9e007d0fb7e2]
    Former-commit-id: e5f0e9a455172c7f6b8b6bc90de4bc2498c3f8ec
    Former-commit-id: be694d7c2afa23f4057aeb1e3d0b5bd179871032 [formerly 4c18f8cd22fa81758e400b02ac53efbb8c365197] [formerly b72161a2150e69d9f36d48f7a2183f903724f22e [formerly 2ff9381807014fc18bc90ff3a5a77097e3f91294]]
    Former-commit-id: 9a65b42a2ddb48246a5b883def9d42dd4d9a0c44 [formerly 8cf9ece6a90e7f91455969bc0e08a5bd5103f248]
    Former-commit-id: 84cc3e31c128c1208e5240a62d483144dc4b9cd0
    Former-commit-id: 058a9784a3242ffa880a071bd3faab45b34173d2

---
+++ Frame/SDK/Base/AFTimerManager.h
@@ -0,0 +1,106 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+#pragma once
+
+#include "AFPlatform.hpp"
+
+class AFTimerManager;
+
+enum class ETimerStatus : uint8_t
+{
+    Pending,
+    Active,
+    Paused,
+    Executing,
+};
+
+struct AFTimerHandle
+{
+public:
+    friend class AFTimerManager;
+    
+    AFTimerHandle()
+        : handle(0)
+    {}
+
+    bool IsValid() const
+    {
+        return handle != 0;
+    }
+
+    void Invalidate()
+    {
+        handle = 0;
+    }
+
+    void MakeValid()
+    {
+        //AFTimerManager::ValidateHandle(*this);
+    }
+
+    bool operator==(const AFTimerHandle& other) const
+    {
+        return handle == other.handle;
+    }
+
+    bool operator!=(const AFTimerHandle& other) const
+    {
+        return handle != other.handle;
+    }
+
+    //std::string ToString()
+    //{
+    //    //TODO
+    //}
+
+private:
+    uint64_t handle;
+};
+
+struct AFTimerData 
+{
+    uint8_t bLoop : 1;
+    ETimerStatus nStatus;
+    float fRate;
+    double dExpire_time;
+    AFTimerHandle xTimerHandle;
+
+    AFTimerData()
+        : bLoop(false)
+        , nStatus(ETimerStatus::Active)
+        , fRate(0)
+        , dExpire_time(0)
+    {}
+
+    bool operator<(const AFTimerData& other) const
+    {
+        return dExpire_time < other.dExpire_time;
+    }
+
+    void clear()
+    {
+        xTimerHandle.Invalidate();
+    }
+};
+
+class AFTimerManager
+{
+    //TODO:
+};
