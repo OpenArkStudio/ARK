fad30d55fc84b141d234a4ee4a76f4f8bf6e7cae
flyicegood <flyicegood@163.com>
2017-08-15 22:49:01 +0800

    modify name
    Former-commit-id: a00e8f2a5a14f30532a3424e9f5dc53949088f5b
    Former-commit-id: e46cbb79a265d01796f815a2d8048a19e78fe08d
    Former-commit-id: 8a027aaba8ee0d3bf702021fc74d469cbed5e058

--- Frame/Server/WorldServer/WorldLogicPlugin/dllmain.cpp
+++ Frame/Server/WorldServer/WorldLogicPlugin/dllmain.cpp
@@ -6,7 +6,7 @@
 #pragma comment( lib, "Core_d.lib" )
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
 ////#pragma comment( lib, "RakNet_LibStatic_Debug_x64.a" )
-#pragma comment( lib, "AFNetEvpp_d.a" )
+#pragma comment( lib, "AFNet_d.a" )
 #pragma comment( lib, "Core_d.a" )
 #pragma comment( lib, "Proto_d.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
@@ -16,12 +16,12 @@
 
 #if ARK_PLATFORM == PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "AFNetEvpp.lib" )
+#pragma comment( lib, "AFNet.lib" )
 #pragma comment( lib, "Core.lib" )
 #pragma comment( lib, "Proto.lib" )
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
 //#pragma comment( lib, "RakNet_LibStatic_Release_x64.a" )
-#pragma comment( lib, "AFNetEvpp.a" )
+#pragma comment( lib, "AFNet.a" )
 #pragma comment( lib, "Core.a" )
 #pragma comment( lib, "Proto.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
