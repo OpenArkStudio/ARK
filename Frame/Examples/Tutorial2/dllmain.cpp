dc802a951d179ee0cd3d1d59872278d0131b1cef
flyicegood <flyicegood@163.com>
2017-08-16 22:49:33 +0800

    rename
    Former-commit-id: 695e1025921a8ce81f4e085103101129ba7adbba
    Former-commit-id: 660e1022c8de2b1af09e65cd621967245902f100
    Former-commit-id: e411c79d32091d43284d0b52e43a85d76e71fde3

--- Frame/Examples/Tutorial2/dllmain.cpp
+++ Frame/Examples/Tutorial2/dllmain.cpp
@@ -3,19 +3,19 @@
 #if ARK_RUN_MODE == ARK_RUN_MODE_DEBUG
 
 #if ARK_PLATFORM == PLATFORM_WIN
-#pragma comment( lib, "Core_d.lib" )
+#pragma comment( lib, "AFCore_d.lib" )
 
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "Core_d.a" )
+#pragma comment( lib, "AFCore_d.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
 #else
 
 #if ARK_PLATFORM == PLATFORM_WIN
-#pragma comment( lib, "Core.lib" )
+#pragma comment( lib, "AFCore.lib" )
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "Core.a" )
+#pragma comment( lib, "AFCore.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
