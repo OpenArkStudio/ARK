73258dde0597dab3f5cb9945ccc07d552d5cca4a
flyicegood <flyicegood@163.com>
2017-08-16 22:49:33 +0800

    rename
    Former-commit-id: 93983a4cd05dc065a9fa3716ab4dd8ac175e058c [formerly 396528768c8af9fc19da3c372a5a9defaf4607d3] [formerly 19d143ba25f856def00c3e4686324296e29ad1e1 [formerly 733799f2b8a6ad1225917c39134f84e6d69f71c2]]
    Former-commit-id: 92913ebdaf8d285239c8ffe4cff1925a9f1f51fb [formerly 60109424ef24ab34af06844268124211beed9914]
    Former-commit-id: b905a27be9bac96e3fd56e43c2537204934a66bf
    Former-commit-id: c5142c65ba62aff4559079f30eb01b8ddd00e7fc

--- Frame/Server/MasterServer/MasterNet_ServerPlugin/dllmain.cpp
+++ Frame/Server/MasterServer/MasterNet_ServerPlugin/dllmain.cpp
@@ -6,16 +6,16 @@
 #pragma comment( lib, "ws2_32" )
 //#pragma comment( lib, "RakNet_LibStatic_Debug_x64.lib" )
 #pragma comment( lib, "AFNet_d.lib" )
-#pragma comment( lib, "Core_d.lib" )
-#pragma comment( lib, "Proto_d.lib" )
+#pragma comment( lib, "AFCore_d.lib" )
+#pragma comment( lib, "AFProto_d.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
 #pragma comment( lib, "event_core.lib" )
 
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
 ////#pragma comment( lib, "RakNet_LibStatic_Debug_x64.a" )
 #pragma comment( lib, "AFNet_d.a" )
-#pragma comment( lib, "Core_d.a" )
-#pragma comment( lib, "Proto_d.a" )
+#pragma comment( lib, "AFCore_d.a" )
+#pragma comment( lib, "AFProto_d.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
@@ -24,15 +24,15 @@
 #if ARK_PLATFORM == PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
 #pragma comment( lib, "AFNet.lib" )
-#pragma comment( lib, "Core.lib" )
-#pragma comment( lib, "Proto.lib" )
+#pragma comment( lib, "AFCore.lib" )
+#pragma comment( lib, "AFProto.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
 #pragma comment( lib, "event_core.lib" )
 
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "AFNet.a" )
-#pragma comment( lib, "Core.a" )
-#pragma comment( lib, "Proto.a" )
+#pragma comment( lib, "AFCore.a" )
+#pragma comment( lib, "AFProto.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
