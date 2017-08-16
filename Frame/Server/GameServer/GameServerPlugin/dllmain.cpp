1d9b88eb425a05d1a0c8827343cd941515833543
flyicegood <flyicegood@163.com>
2017-08-16 22:49:33 +0800

    rename
    Former-commit-id: 695e1025921a8ce81f4e085103101129ba7adbba
    Former-commit-id: df4c7ba33b4af4e5439798292e490a399351d99f [formerly 501e0a6c1b278968893b2eb5440eac71a948abfa] [formerly f17ea2fd5841b327246ee25368680614d919ca9a [formerly 999fc214be0608d12627d06e504ae005a071f072]]
    Former-commit-id: 41ab7fb701f80fbb618c0b1f803979bd88285984 [formerly e0eb552c42541e2fa08ae26399e5a613c80a7faa]
    Former-commit-id: 9443e629f0ceba73d6e785f93bfa5de382648060
    Former-commit-id: bcea4c4472a3f002969dc73dd38412ab0e15b596

--- Frame/Server/GameServer/GameServerPlugin/dllmain.cpp
+++ Frame/Server/GameServer/GameServerPlugin/dllmain.cpp
@@ -4,15 +4,15 @@
 
 #if ARK_PLATFORM == PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "Core_d.lib" )
-#pragma comment( lib, "Proto_d.lib" )
+#pragma comment( lib, "AFCore_d.lib" )
+#pragma comment( lib, "AFProto_d.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
 //#pragma comment( lib, "libglog_static_d.lib" )
 
 
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "Core_d.a" )
-#pragma comment( lib, "Proto_d.a" )
+#pragma comment( lib, "AFCore_d.a" )
+#pragma comment( lib, "AFProto_d.a" )
 //#pragma comment( lib, "libglog_d.lib" )
 
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
@@ -21,13 +21,13 @@
 #else
 
 #if ARK_PLATFORM == PLATFORM_WIN
-#pragma comment( lib, "Core.lib" )
-#pragma comment( lib, "Proto.lib" )
+#pragma comment( lib, "AFCore.lib" )
+#pragma comment( lib, "AFProto.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
 
 #elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "Core.a" )
-#pragma comment( lib, "Proto.a" )
+#pragma comment( lib, "AFCore.a" )
+#pragma comment( lib, "AFProto.a" )
 #pragma comment( lib, "libprotobuf.a" )
 #elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
