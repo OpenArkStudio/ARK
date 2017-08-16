d688a3ada1dd31e071d02167c60e16c710d86385
NickYang <362148418@qq.com>
2017-08-16 15:03:53 +0800

    modify dll name
    Former-commit-id: 9dcf4b8736882898451bbaf0eeb03568a6f74d44 [formerly c44cfb063cac4f8f46b9f71e9101bafdaae16e6e] [formerly 04fe0bf33a6b22fba484a658db4bd15c8fc4a951] [formerly 9b42282b1fbaa6dd7bb8968219d2daca823bf468 [formerly 841df3be1d815219040617bb92865e1d01decb85]] [formerly e0197908bd95762bffa0e0eb6e2eb54dac4035a5 [formerly 9d49067444b742bb4b50d9fb552b201f406a617d] [formerly a7a0730fbee38e3f99180dc29b8e73cc6ff6634e [formerly 7b27c69ed7c7763480cf9b6002f76233d27f5073]]] [formerly 4bf84ab210bd880a09a0df49ed47921bca868221]
    Former-commit-id: 10aa5f99addeaa34d986aed2f9e1c7dd416a22a7
    Former-commit-id: d5cd97d66f3c90d9ef0498486d36964ca222da56 [formerly 290f3204a43a59308c2819ab89ad83794b213fa9] [formerly e7ae56bda71ebacf48131213c316bb4e272ce904 [formerly b525c0054bd31ec91aab83e92e7d2069d63e97d7]]
    Former-commit-id: 059f68f1ee3b3f5eb119ace7ce43dee3f02500a2 [formerly e01028a8573972b5489b31e24fd3fd18e8d865c4]
    Former-commit-id: 18645546b74b80f93f0daf2138a03c803084c114
    Former-commit-id: 162682a41eb1b8ea54461eb3b15068cbcbc3d1aa

--- Frame/SDK/Base/AFTime.hpp
+++ Frame/SDK/Base/AFTime.hpp
@@ -1,16 +1,26 @@
-#ifndef NF_TIME_H
-#define NF_TIME_H
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+#pragma once
 
-#include <vector>
-#include <string>
 #include "AFMacros.hpp"
 #include "AFSingleton.hpp"
-
-#pragma  once
-
-//某点清空某些东西
-#define CLEAR_HOUR 5
-
 
 class AFCTimeBase :
     public AFSingleton<AFCTimeBase>
@@ -799,5 +809,3 @@ private:
     int64_t mnTime;
     int mnTimeZone;  //默认给UTC+8
 };
-
-#endif // NF_TIME_H
