a82a3704aa2cd8d0c35f3c4e5822078648a44a56
flyicegood <flyicegood@163.com>
2017-08-17 23:00:18 +0800

    rename plugin name
    Former-commit-id: 5645d2b3cdd515bd1ff3f43013f58c7fe06e85cc [formerly cbef3caeafee884a586803fa3f2d5857786d5fbc] [formerly b3d684d0562de231aee1cc8e73b30507902ae834] [formerly 604b4d0f66270ae4892e46810a6927bf53237b29]
    Former-commit-id: 4966f3f31ee6e867f58a0ba7a563c0b9659b88f1
    Former-commit-id: b4c44b8e3ccf138c67f266ee7938afa1e8c34e8e
    Former-commit-id: 8d9e564da1994820e5231279c5ff2ffc6529e20f

--- Frame/Server/ProxyServer/ProxyServerNet_ClientPlugin/AFCProxyServerToWorldModule.cpp
+++ Frame/Server/ProxyServer/ProxyServerNet_ClientPlugin/AFCProxyServerToWorldModule.cpp
@@ -7,7 +7,7 @@
 // -------------------------------------------------------------------------
 
 #include "AFCProxyServerToWorldModule.h"
-#include "AFProxyServerNet_ClientPlugin.h"
+#include "AFProxyNetClientPlugin.h"
 #include "SDK/Core/AFIHeartBeatManager.h"
 #include "SDK/Core/AFCHeartBeatManager.h"
 #include "SDK/Interface/AFIClassModule.h"
