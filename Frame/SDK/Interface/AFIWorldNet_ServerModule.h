cd642a9bec207892e23f588c89f07c4850d98896
flyicegood <flyicegood@163.com>
2017-08-20 14:31:30 +0800

    modify name
    Former-commit-id: fd37afcc86d6ffeb57eea8b9abea0b7030bfa69f

--- Frame/SDK/Interface/AFIWorldNet_ServerModule.h
+++ Frame/SDK/Interface/AFIWorldNet_ServerModule.h
@@ -11,7 +11,7 @@
 
 #include <iostream>
 #include "AFIModule.h"
-#include "SDK/Proto/NFMsgDefine.h"
+#include "SDK/Proto/AFMsgDefine.h"
 #include "AFINetServerModule.h"
 
 class AFIWorldNet_ServerModule
@@ -19,13 +19,13 @@ class AFIWorldNet_ServerModule
 {
 
 public:
-    virtual bool SendMsgToGame(const int nGameID, const NFMsg::EGameMsgID eMsgID, google::protobuf::Message& xData, const AFGUID nPlayer = AFGUID()) = 0;
+    virtual bool SendMsgToGame(const int nGameID, const AFMsg::EGameMsgID eMsgID, google::protobuf::Message& xData, const AFGUID nPlayer = AFGUID()) = 0;
     virtual int OnObjectListEnter(const AFIDataList& self, const AFIDataList& argVar) = 0;
     virtual int OnObjectListLeave(const AFIDataList& self, const AFIDataList& argVar) = 0;
     virtual int OnPropertyEnter(const AFIDataList& argVar, const AFIDataList& argGameID, const AFGUID& self) = 0;
     virtual int OnRecordEnter(const AFIDataList& argVar, const AFIDataList& argGameID, const AFGUID& self) = 0;
-    virtual bool SendMsgToGame(const AFIDataList& argObjectVar, const AFIDataList& argGameID,  const NFMsg::EGameMsgID eMsgID, google::protobuf::Message& xData) = 0;
-    virtual bool OnRecordEnterPack(ARK_SHARE_PTR<AFIRecord> pRecord, NFMsg::ObjectRecordBase* pObjectRecordBase) = 0;
+    virtual bool SendMsgToGame(const AFIDataList& argObjectVar, const AFIDataList& argGameID,  const AFMsg::EGameMsgID eMsgID, google::protobuf::Message& xData) = 0;
+    virtual bool OnRecordEnterPack(ARK_SHARE_PTR<AFIRecord> pRecord, AFMsg::ObjectRecordBase* pObjectRecordBase) = 0;
 
     virtual ARK_SHARE_PTR<ServerData> GetSuitProxyForEnter() = 0;
     virtual AFINetServerModule* GetNetModule() = 0;
