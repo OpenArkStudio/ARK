06437c7da20b7d2b33400ddf6acf02c1457f23d2
flyicegood <flyicegood@163.com>
2017-08-20 15:32:02 +0800

    rename
    Former-commit-id: 74b53a0bcff81794d1eb85c2b11a44f66caeae64
    Former-commit-id: dda93829f31e0dcc74a2714469944eb49fedf393
    Former-commit-id: cadc4a729111d157c349c66d13fdc2a33de42a8b

--- Frame/SDK/Net/AFCNetServer.cpp
+++ Frame/SDK/Net/AFCNetServer.cpp
@@ -253,7 +253,7 @@ bool AFCNetServer::CloseNetObject(const AFGUID& xClientID)
 bool AFCNetServer::DismantleNet(NetObject* pObject)
 {
     int len = pObject->GetBuffLen();
-    for(; pObject->BuffChange() && len > AFIMsgHead::NF_Head::NF_HEAD_LENGTH;)
+    for(; pObject->BuffChange() && len > AFIMsgHead::AF_Head::NF_HEAD_LENGTH;)
     {
         AFCMsgHead xHead;
         int nMsgBodyLength = DeCode(pObject->GetBuff(), len, xHead);
@@ -262,14 +262,14 @@ bool AFCNetServer::DismantleNet(NetObject* pObject)
             /*int nRet = 0;
             if(mRecvCB)
             {
-                mRecvCB(xHead.GetMsgID(), pObject->GetBuff() + AFIMsgHead::NF_Head::NF_HEAD_LENGTH, nMsgBodyLength, pObject->GetClientID());
+                mRecvCB(xHead.GetMsgID(), pObject->GetBuff() + AFIMsgHead::AF_Head::NF_HEAD_LENGTH, nMsgBodyLength, pObject->GetClientID());
             }*/
             MsgFromNetInfo* pNetInfo = new  MsgFromNetInfo(pObject->GetConnPtr());
             pNetInfo->xHead = xHead;
             pNetInfo->nType = RECIVEDATA;
-            pNetInfo->strMsg.append(pObject->GetBuff() + AFIMsgHead::NF_Head::NF_HEAD_LENGTH, nMsgBodyLength);
+            pNetInfo->strMsg.append(pObject->GetBuff() + AFIMsgHead::AF_Head::NF_HEAD_LENGTH, nMsgBodyLength);
             pObject->mqMsgFromNet.Push(pNetInfo);
-            pObject->RemoveBuff(nMsgBodyLength + AFIMsgHead::NF_Head::NF_HEAD_LENGTH);
+            pObject->RemoveBuff(nMsgBodyLength + AFIMsgHead::AF_Head::NF_HEAD_LENGTH);
             len = pObject->GetBuffLen();
         }
         break;
@@ -309,7 +309,7 @@ bool AFCNetServer::SendMsgWithOutHead(const int16_t nMsgID, const char* msg, con
 {
     std::string strOutData;
     int nAllLen = EnCode(nMsgID, msg, nLen, strOutData);
-    if(nAllLen == nLen + AFIMsgHead::NF_Head::NF_HEAD_LENGTH)
+    if(nAllLen == nLen + AFIMsgHead::AF_Head::NF_HEAD_LENGTH)
     {
         return SendMsg(strOutData.c_str(), strOutData.length(), xClientID);
     }
@@ -321,7 +321,7 @@ bool AFCNetServer::SendMsgToAllClientWithOutHead(const int16_t nMsgID, const cha
 {
     std::string strOutData;
     int nAllLen = EnCode(nMsgID, msg, nLen, strOutData);
-    if(nAllLen == nLen + AFIMsgHead::NF_Head::NF_HEAD_LENGTH)
+    if(nAllLen == nLen + AFIMsgHead::AF_Head::NF_HEAD_LENGTH)
     {
         return SendMsgToAllClient(strOutData.c_str(), strOutData.length());
     }
@@ -335,29 +335,29 @@ int AFCNetServer::EnCode(const uint16_t unMsgID, const char* strData, const uint
     xHead.SetMsgID(unMsgID);
     xHead.SetBodyLength(unDataLen);
 
-    char szHead[AFIMsgHead::NF_Head::NF_HEAD_LENGTH] = { 0 };
+    char szHead[AFIMsgHead::AF_Head::NF_HEAD_LENGTH] = { 0 };
     xHead.EnCode(szHead);
 
     strOutData.clear();
-    strOutData.append(szHead, AFIMsgHead::NF_Head::NF_HEAD_LENGTH);
+    strOutData.append(szHead, AFIMsgHead::AF_Head::NF_HEAD_LENGTH);
     strOutData.append(strData, unDataLen);
 
-    return xHead.GetBodyLength() + AFIMsgHead::NF_Head::NF_HEAD_LENGTH;
+    return xHead.GetBodyLength() + AFIMsgHead::AF_Head::NF_HEAD_LENGTH;
 }
 
 int AFCNetServer::DeCode(const char* strData, const uint32_t unAllLen, AFCMsgHead& xHead)
 {
-    if(unAllLen < AFIMsgHead::NF_Head::NF_HEAD_LENGTH)
+    if(unAllLen < AFIMsgHead::AF_Head::NF_HEAD_LENGTH)
     {
         return -1;
     }
 
-    if(AFIMsgHead::NF_Head::NF_HEAD_LENGTH != xHead.DeCode(strData))
+    if(AFIMsgHead::AF_Head::NF_HEAD_LENGTH != xHead.DeCode(strData))
     {
         return -2;
     }
 
-    if(xHead.GetBodyLength() > (unAllLen - AFIMsgHead::NF_Head::NF_HEAD_LENGTH))
+    if(xHead.GetBodyLength() > (unAllLen - AFIMsgHead::AF_Head::NF_HEAD_LENGTH))
     {
         return -3;
     }
