d7114fdc603f48933e7a77e3967d4202e9364ccb
flyicegood <flyicegood@163.com>
2017-08-20 15:32:02 +0800

    rename
    Former-commit-id: 74b53a0bcff81794d1eb85c2b11a44f66caeae64
    Former-commit-id: 370b16604f01a4602bff9883d6ff2e2f9486e8e8 [formerly 8ec280329151de020b0011f4fc03f55940a906fc] [formerly c6b4691d8dd4060af30443656804515a15caae89 [formerly c2cb45de0d63d41ab7a40f99f9bcbef4728a9e82]]
    Former-commit-id: 6d09a56d6cc7789083adf025a778029b2a759260 [formerly 31104db4704a766a9b024304dbbc3622b78446b9]
    Former-commit-id: 731aec081612ee3f957ec1b6607c0fa96bd83310
    Former-commit-id: cce7e969367f4d01654a55f4072f078f22873bf9

--- Frame/Server/WorldServer/WorldLogicPlugin/dllmain.cpp
+++ Frame/Server/WorldServer/WorldLogicPlugin/dllmain.cpp
@@ -4,12 +4,12 @@
 
 #if ARK_PLATFORM == PLATFORM_WIN
 #pragma comment( lib, "AFCore_d.lib" )
-#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
+#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == AF_PLATFORM_ANDROID
 ////#pragma comment( lib, "RakNet_LibStatic_Debug_x64.a" )
 #pragma comment( lib, "AFNet_d.a" )
 #pragma comment( lib, "AFCore_d.a" )
 #pragma comment( lib, "AFProto_d.a" )
-#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
+#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == AF_PLATFORM_APPLE_IOS
 #endif
 
 #else
@@ -19,12 +19,12 @@
 #pragma comment( lib, "AFNet.lib" )
 #pragma comment( lib, "AFCore.lib" )
 #pragma comment( lib, "AFProto.lib" )
-#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == NF_PLATFORM_ANDROID
+#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == AF_PLATFORM_ANDROID
 //#pragma comment( lib, "RakNet_LibStatic_Release_x64.a" )
 #pragma comment( lib, "AFNet.a" )
 #pragma comment( lib, "AFCore.a" )
 #pragma comment( lib, "AFProto.a" )
-#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == NF_PLATFORM_APPLE_IOS
+#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == AF_PLATFORM_APPLE_IOS
 #endif
 
 #endif
