7722bfcc8d3afc111c7834f058b2a5d4e61f6e11
flyicegood <flyicegood@163.com>
2017-08-25 19:48:49 +0800

    add head
    Former-commit-id: 108d5776c84edfa2b0b2eb20e3b9fbd80a79a1d7
    Former-commit-id: 157843088a27b1f9ca7da000acb1c6e21a7f2de8

--- Frame/SDK/Core/AFIComponent.h
+++ Frame/SDK/Core/AFIComponent.h
@@ -1,11 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName         :    AFIComponent.h
-//    @Author           :    Ark Game Tech
-//    @Date             :    2014-02-17
-//    @Module           :    AFIComponent
-//
-// -----------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFIComponent.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFIComponent                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFIComponent                                                  *
+*****************************************************************************/
 #ifndef AFI_COMPONENT_H
 #define AFI_COMPONENT_H
 
@@ -255,3 +273,4 @@ private:
 };
 
 #endif
+
---
+++ Frame/Server/MasterServer/MasterNet_ServerPlugin/AFMasterNetServerPlugin.h
@@ -0,0 +1,57 @@
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFMasterNetServerPlugin.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFMasterNetServerPlugin                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFMasterNetServerPlugin                                                  *
+*****************************************************************************/
+
+
+///
+#include "SDK/Interface/AFIPlugin.h"
+#include "SDK/Interface/AFIPluginManager.h"
+
+#ifdef AFMASTERNET_SERVERPLUGIN_EXPORTS
+#define AFMASTERNET_SERVERPLUGIN_API __declspec(dllexport)
+#else
+#define AFMASTERNET_SERVERPLUGIN_API __declspec(dllimport)
+#endif
+
+
+//////////////////////////////////////////////////////////////////////////
+class AFMasterNetServerPlugin : public AFIPlugin
+{
+public:
+    AFMasterNetServerPlugin(AFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+    virtual const int GetPluginVersion();
+
+    virtual const std::string GetPluginName();
+
+    virtual void Install();
+
+    virtual void Uninstall();
+};
+
+
