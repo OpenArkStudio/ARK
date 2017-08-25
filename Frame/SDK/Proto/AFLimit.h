5243d9834c8e2b1c98d51b319e73a4e17f31e417
flyicegood <flyicegood@163.com>
2017-08-25 19:48:49 +0800

    add head
    Former-commit-id: 81b41102b32e81c7ed10b228cd6999f124880fe2 [formerly 473e14ec80f53149dbd7f81aaa8b25f413a12fe9]
    Former-commit-id: 006e38e7a0abd26790a804a65cef13176e3c2143

--- Frame/SDK/Proto/AFLimit.h
+++ Frame/SDK/Proto/AFLimit.h
@@ -1,11 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName			:    NFLimit.h
-//    @Author      :    Ark Game Tech
-//    @Date    :    2010-10-5 12:05
-//    @Module    :    极限定义值
-//
-// -------------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFLimit.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFLimit                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFLimit                                                  *
+*****************************************************************************/
 #ifndef NF_LIMIT_H
 #define NF_LIMIT_H
 
--- Frame/SDK/Proto/NFProtocolDefine.hpp
+++ Frame/SDK/Proto/NFProtocolDefine.hpp
@@ -1,9 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName         :    NFProtocolDefine.hpp
-//    @Author           :    Ark Game
-//    @Module           :    NFProtocolDefine
-// -------------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  NFProtocolDefine.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    NFProtocolDefine                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     NFProtocolDefine                                                  *
+*****************************************************************************/
 #ifndef NF_PR_NAME_HPP
 #define NF_PR_NAME_HPP
 
