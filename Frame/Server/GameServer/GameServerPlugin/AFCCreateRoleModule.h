7efa3a38d349627d42464d6f938dfbe2dda984ee
flyicegood <flyicegood@163.com>
2017-08-25 19:48:49 +0800

    add head

--- Frame/Server/GameServer/GameServerPlugin/AFCCreateRoleModule.h
+++ Frame/Server/GameServer/GameServerPlugin/AFCCreateRoleModule.h
@@ -1,11 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName      :    AFCCreateRoleModule.h
-//    @Author           :    Ark Game Tech
-//    @Date             :    2013-03-29
-//    @Module           :    AFCCreateRoleModule
-//
-// -------------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFCCreateRoleModule.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFCCreateRoleModule                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFCCreateRoleModule                                                  *
+*****************************************************************************/
 #ifndef _AFC_CREATEROLE_MODULE_H_
 #define _AFC_CREATEROLE_MODULE_H_
 
@@ -64,3 +82,4 @@ private:
 };
 
 #endif
+
--- Frame/Server/WorldServer/WorldLogicPlugin/AFWorldLogicPlugin.h
+++ Frame/Server/WorldServer/WorldLogicPlugin/AFWorldLogicPlugin.h
@@ -1,11 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName         :    AFWorldLogicPlugin.h
-//    @Author           :    Ark Game Tech
-//    @Date             :    2012-07-14 08:51
-//    @Module           :   AFWorldLogicPlugin
-//
-// -------------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFWorldLogicPlugin.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFWorldLogicPlugin                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFWorldLogicPlugin                                                  *
+*****************************************************************************/
 #ifndef AFC_WORLDLOGICPLUGIN_MODULE_H
 #define AFC_WORLDLOGICPLUGIN_MODULE_H
 
