a952807bc7597b08078c35e7c083cca4d6519c81
flyicegood <flyicegood@163.com>
2017-08-25 19:48:49 +0800

    add head
    Former-commit-id: 473e14ec80f53149dbd7f81aaa8b25f413a12fe9
    Former-commit-id: d889d84aa599bc6cb2d0b03d22ff72143773e3a5
    Former-commit-id: 992dc38385df668369b9e809e13ab645d43f250c

--- Frame/SDK/Proto/AFMsgDefine.h
+++ Frame/SDK/Proto/AFMsgDefine.h
@@ -1,11 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName     :    AFMsgDefine.h
-//    @Author       :    Ark Game Tech
-//    @Date         :    2010-10-5 12:05
-//    @Module       :    消息协议声明
-//
-// -------------------------------------------------------------------------
-
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @FileName  AFMsgDefine.h                                                *
+// * @Author    Ark Game Tech                                                *
+// * @Date      2015-12-15                                                   *
+// * @Module    AFMsgDefine                                                  *
+// * @email     ArkGameTech@163.com                                          *
+// * @brief     AFMsgDefine                                                  *
+*****************************************************************************/
 #ifndef AF_MSGDEFINE_H
 #define AF_MSGDEFINE_H
 
@@ -22,3 +40,4 @@
 #include "AFMsgMysql.pb.h"
 
 #endif
+
