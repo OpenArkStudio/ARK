ff149be6d0e5dc80cd01a1656ac7487060efb5d7
NickYang <362148418@qq.com>
2017-08-29 20:31:38 +0800

    fix static linked libraries in dllmain.cpp
    Former-commit-id: e8e25b423b16b9d09c54371557695ce963e35e2c [formerly d9a653fc042f73493497e8652e978acf95195a32] [formerly 45928f6e949acb39a3fd5ad1c2220fb1edcca652] [formerly 4a4066c8a749b09235072f913c487f21a4a0ec29 [formerly ccffcd57be3ac7588e865cdf4c2291d48ba5b506]] [formerly 3d229b94e7439f2bc22a7b6bfe8d1e8907b51fed [formerly f6254fe2801c460435b02aa3c903174d73696ab8] [formerly d547ed7640b0571ef350deb697f6a317179763e0 [formerly 6fa707bcf44d5572955519199a4cfdf0ba4ed8dd]]] [formerly 6ecfd0c189886704a5878aad2a54dbe3af91dc1b]
    Former-commit-id: 2a7a6fbb8415b9627d2a123d2fd276ef61bd2498
    Former-commit-id: 971bd6a8839733a7fd1fedbb8e9d304fb2ea6bf2 [formerly a1c6b601c69f401ddd656023716faeffc0812f62] [formerly a1ece1a2eed4e3da2d930952bfc1a0a6fcac56e5 [formerly b3c895a1ccd5b2753725b46216547ccbc842306b]]
    Former-commit-id: b2b88d068b8917776ea4a29f2f598907df03662f [formerly 5d64cef1fc52374f150bbc4f56d545401644ff85]
    Former-commit-id: 27bf4fb6b29eb5dc3f312f5a3cdf08cf9064fc79
    Former-commit-id: e0a17d2dde485cd1d83628cf6ed320930b9f2576

--- Frame/SDK/Base/AFCConsistentHash.hpp
+++ Frame/SDK/Base/AFCConsistentHash.hpp
@@ -1,35 +1,25 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFCConsistentHash.h                                                *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCConsistentHash                                                  *
-*****************************************************************************/
-#ifndef AFC_CONSISTENT_HASH_H
-#define AFC_CONSISTENT_HASH_H
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
-#include <map>
-#include <string>
-#include <list>
-#include <functional>
-#include <algorithm>
+#pragma once
+
 #include "SDK/Base/AFPlatform.hpp"
 #include "common/crc32.hpp"
 #include "AFMacros.hpp"
@@ -773,8 +763,3 @@ private:
     /** @brief   The hasher. */
     AFIHasher* m_pHasher;
 };
-
-
-#endif
-
-
