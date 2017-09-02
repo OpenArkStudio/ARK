8a1b4b978a273ccb9d994b0861e61fc170fe04e8
NickYang <362148418@qq.com>
2017-09-02 17:14:46 +0800

    add new AFRecord
    Former-commit-id: 7d46ecb83514aec0a076638d875c4be6fd6a3666

--- Frame/SDK/Core/AFRecord.h
+++ Frame/SDK/Core/AFRecord.h
@@ -1,87 +1,110 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFRecord.h                                                *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFRecord                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #pragma once
 
 #include "SDK/Base/AFCDataList.h"
 #include "SDK/Base/AFArrayPod.hpp"
 #include "SDK/Base/AFCoreDef.hpp"
 #include "SDK/Base/AFString.hpp"
-#include "SDK/Base/AFIData.h"
+#include "SDK/Base/AFCData.h"
 
 using namespace ArkFrame;
 
 class AFRecord
 {
 private:
-    using row_data_t = AFIData;
+    using RowData = AFCData;
+    enum RECORD_FEATURE
+    {
+        RF_PUBLIC       = 0, //send to others
+        RF_PRIVATE      = 1, //send to self
+        RF_REAL_TIME    = 2, //send real-time when changed
+        RF_SAVE         = 3, //if need save to database
+    };
 
 public:
     AFRecord();
     virtual ~AFRecord();
 
+    void SetName(const char* value);
     const char* GetName() const;
 
-    virtual int GetCols() const;
+    int GetRowCount() const;
 
-    virtual int GetRows() const;
+    void SetColCount(size_t value);
+    int GetColCount() const;
+    
+    bool SetColType(size_t index, int type);
+    int GetColType(int col) const;
 
-    virtual int GetRowMax() const;
+    bool AddRow(size_t row);
+    bool AddRow(size_t row, const AFIDataList& data);
 
-    virtual int GetColType(int col) const;
+    bool DeleteRow(size_t row);
 
-    virtual bool AddRow(const int row);
-    virtual bool AddRow(const int row, const AFIDataList& data);
+    void Clear();
+    
+    bool SetValue(size_t row, size_t col, const AFIData& value);
+    bool SetBool(size_t row, size_t col, const bool value);
+    bool SetInt(size_t row, size_t col, const int value);
+    bool SetInt64(size_t row, size_t col, const int64_t value);
+    bool SetFloat(size_t row, size_t col, const float value);
+    bool SetDouble(size_t row, size_t col, const double value);
+    bool SetString(size_t row, size_t col, const char* value);
+    bool SetObject(size_t row, size_t col, const AFGUID& value);
 
-    virtual bool SetInt(const int nRow, const int nCol, const int64_t value);
-    virtual bool SetDouble(const int nRow, const int nCol, const double value);
-    virtual bool SetString(const int nRow, const int nCol, const std::string& value);
-    virtual bool SetObject(const int nRow, const int nCol, const AFGUID& value);
+    bool GetValue(size_t row, size_t col, AFIData& value);
+    bool GetBool(size_t row, size_t col);
+    int GetInt(size_t row, size_t col);
+    int64_t GetInt64(size_t row, size_t col);
+    float GetFloat(size_t row, size_t col);
+    double GetDouble(size_t row, size_t col);
+    const char* GetString(size_t row, size_t col);
+    const AFGUID& GetObject(size_t row, size_t col);
 
-    virtual int64_t GetInt(const int nRow, const int nCol) const;
-    virtual double GetDouble(const int nRow, const int nCol) const;
-    virtual const std::string& GetString(const int nRow, const int nCol) const;
-    virtual const AFGUID& GetObject(const int nRow, const int nCol) const;
+    const char* GetStringValue(size_t row, size_t col);
 
-    virtual int FindInt(const int nCol, const int64_t value, AFIDataList& varResult);
-    virtual int FindDouble(const int nCol, const double value, AFIDataList& varResult);
-    virtual int FindString(const int nCol, const std::string& value, AFIDataList& varResult);
-    virtual int FindObject(const int nCol, const AFGUID& value, AFIDataList& varResult);
+    int FindRow(size_t col, const AFIData& key, size_t begin_row = 0);
+    int FindBool(size_t col, const bool key, size_t begin_row = 0);
+    int FindInt(size_t col, const int key, size_t begin_row = 0);
+    int FindInt64(size_t col, const int64_t key, size_t begin_row = 0);
+    int FindFloat(size_t col, const float key, size_t begin_row = 0);
+    int FindDouble(size_t col, const double key, size_t begin_row = 0);
+    int FindString(size_t col, const char* key, size_t begin_row = 0);
+    int FindObject(size_t col, const AFGUID& key, size_t begin_row = 0);
 
-    virtual bool QueryRow(const int nRow, AFIDataList& varList);
+    bool QueryRow(const int nRow, AFIDataList& varList);
+
+private:
+    void ReleaseRow(RowData* row_data, size_t col_num);
+    void ReleaseAll();
 
 protected:
 
-    RecordName mstrName;
-    ArraryPod<int, 1, CoreAlloc> mxColTypes;
-    ArraryPod<row_data_t*, 1, CoreAlloc> mxRowDatas;
+    RecordName mstrName;                            //record name
+    int8_t feature;                                 //record feature
+    ArraryPod<int, 1, CoreAlloc> mxColTypes;        //record type array
+    ArraryPod<RowData*, 1, CoreAlloc> mxRowDatas;   //record data array
 
-    bool mbSave;
-    bool mbPublic;
-    bool mbPrivate;
-    bool mbRealtime;
-
-    //std::vector<RECORD_EVENT_FUNCTOR_PTR> mtRecordCallback;
+    //will be added
+    //record cb
 };
 
