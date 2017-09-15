9d2fe9facacfa8f876f59eb48e0d9ddddc64c151
flyicegood <flyicegood@163.com>
2017-09-15 16:45:28 +0800

    add Tutorial2Plugin file

---
+++ Frame/Examples/Tutorial2/Tutorial2Plugin.h
@@ -0,0 +1,48 @@
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @file      Tutorial2.h                                                *
+// * @author    Ark Game Tech                                                *
+// * @date      2015-12-15                                                   *
+// * @brief     Tutorial2                                                  *
+*****************************************************************************/
+#ifndef NF_TUTORIAL2_H
+#define NF_TUTORIAL2_H
+
+///
+#include "SDK/Interface/AFIPlugin.h"
+#include "SDK/Interface/AFIPluginManager.h"
+
+class Tutorial2Plugin : public AFIPlugin
+{
+public:
+    Tutorial2Plugin(AFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+    virtual const int GetPluginVersion();
+
+    virtual const std::string GetPluginName();
+
+    virtual void Install();
+
+    virtual void Uninstall();
+};
+#endif
+
