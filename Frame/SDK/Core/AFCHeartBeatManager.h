560daed2a0ade6fa90fcd086e32cac67d434d67b
NickYang <362148418@qq.com>
2017-09-25 20:04:28 +0800

    fix compiling errors
    Former-commit-id: 931af66d49c2352eb4652af179bccbd6c6cb6a2f
    Former-commit-id: bad53ebe3029dfa113e324439a7ea6de539d8987
    Former-commit-id: ebc8787d9e093bc8b209d489f31d0e56224aaa39

--- Frame/SDK/Core/AFCHeartBeatManager.h
+++ Frame/SDK/Core/AFCHeartBeatManager.h
@@ -26,6 +26,7 @@
 #include "SDK/Base/AFIData.h"
 #include "AFIHeartBeatManager.h"
 #include "SDK/Base/AFMapEx.h"
+#include "SDK/Base/AFArrayMap.hpp"
 
 using namespace ArkFrame;
 
@@ -60,9 +61,10 @@ protected:
 
     NFList<std::string> mRemoveListEx;
     std::list<AFCHeartBeatElement> mAddListEx;
-    AFMapEx<std::string, AFCHeartBeatElement> mHeartBeatElementMapEx;
+    //AFMapEx<std::string, AFCHeartBeatElement> mHeartBeatElementMapEx;
+    AFArrayMap<std::string, AFCHeartBeatElement> mHeartBeatElementMapEx;
 
-    std::multimap<int64_t, ARK_SHARE_PTR<AFCHeartBeatElement>> mTimeList;
+    std::multimap<int64_t, AFCHeartBeatElement*> mTimeList;
     AFGUID mTimerIDIndex;
 };
 
