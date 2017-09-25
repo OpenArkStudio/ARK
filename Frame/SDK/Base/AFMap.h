df8cc64fe2c377866dfa4585f58de0bdabd24390
NickYang <362148418@qq.com>
2017-09-25 19:42:39 +0800

    add assert macros replace some AFMapEx with AFArrayMap
    Former-commit-id: 33fddc968942e7dba2ad9d63327473bbb51a6ec7 [formerly b154ff41c02f101197eb752bd6ea6197caca6a93] [formerly ea13a18cb994ea116886110d5f324be813640085] [formerly 7f9ece241e2e3140d908613ea6bce4d8efdd8fc4 [formerly d2606efb362dd79fdf781f122dc1dc2203868d8c]] [formerly c4c267c145580795c1087b19e5454bd13c12f10b [formerly db44b225cc3b609284d944577540fc6c61aafb9a] [formerly c697e697164a86a97d06ae338448038c543c7eea [formerly ac6a9c9e559728eff460eca9f6ba42ac20df48db]]] [formerly bc12953c4a961213ce8a394f0f5da40f01d9b422]
    Former-commit-id: 152ae20539838bd7746d9d976996211f19666767
    Former-commit-id: 467f1adb495f9471acc3dc9dd08e0cd3fc437e56 [formerly d5d26e5ee9b144c16076cc7b449746b5a72a20cd] [formerly 0a5851dc5a3b6e92f9d273cfe05d2614e6e20402 [formerly 4712dc87528738fb835fd437d7c3e16b513d5147]]
    Former-commit-id: 34740f1ec9d578ac1bd2e893c47ad2017f6ba490 [formerly 4be3d82b809627ccdaa697c43b0f5896e113f33f]
    Former-commit-id: 80a3c7638166ceb812e884d3e306f4d619438626
    Former-commit-id: 420fc89e28fe873ecd264f3e77ac8860b97c2697

--- Frame/SDK/Base/AFMap.h
+++ Frame/SDK/Base/AFMap.h
@@ -18,112 +18,36 @@
 *
 */
 
-#ifndef AF_MAP_H
-#define AF_MAP_H
+#pragma once
 
-#include <map>
-#include <list>
-#include <string>
-#include <iostream>
-#include <typeinfo>
-#include "SDK/Base/AFMapEx.h"
-
-/**
- * @class   NFMap
- *
- * @brief   A nf map.
- *
- * @author  flyicegood
- * @date    2016/11/22
- *
- * @tparam  T   Generic type parameter.
- * @tparam  TD  Type of the td.
- */
+#include "AFPlatform.hpp"
+#include "AFMacros.hpp"
 
 template <typename T, typename TD>
-class NFMap
+class AFMap
 {
 public:
+    typedef std::map<T, TD*> map_data;
 
-    /**
-     * @typedef std::map<T, TD*> NFMapOBJECT
-     *
-     * @brief   Defines an alias representing the nf map object.
-     */
-
-    typedef std::map<T, TD*> NFMapOBJECT;
-
-    /**
-     * @fn  NFMap::NFMap()
-     *
-     * @brief   Default constructor.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     */
-
-    NFMap() {};
-
-    /**
-     * @fn  virtual NFMap::~NFMap()
-     *
-     * @brief   Destructor.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     */
-
-    virtual ~NFMap()
-    {
-        //mObjectList.clear();
-        //新的NFMap只管理元素，不管理元素所占的内存，谁new 谁delete
-        //DeleteAllElement();
-    };
-
-    /**
-     * @fn  virtual bool NFMap::AddElement(const T& name, TD* data)
-     *
-     * @brief   Adds an element to 'data'.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @param           name    The name.
-     * @param [in,out]  data    If non-null, the data.
-     *
-     * @return  True if it succeeds, false if it fails.
-     */
+    AFMap() {}
+    virtual ~AFMap() {}
 
     virtual bool AddElement(const T& name, TD* data)
     {
-        typename NFMapOBJECT::iterator itr = mObjectList.find(name);
+        typename map_data::iterator itr = mObjectList.find(name);
         if(itr == mObjectList.end())
         {
-            mObjectList.insert(typename NFMapOBJECT::value_type(name, data));
-            // mObjectList[name] = data;
+            mObjectList.insert(typename map_data::value_type(name, data));
             return true;
         }
 
         return false;
     }
 
-    /**
-     * @fn  virtual TD* NFMap::RemoveElement(const T& name)
-     *
-     * @brief   Removes the element described by name.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @param   name    The name.
-     *
-     * @return  Null if it fails, else a pointer to a TD.
-     */
-
     virtual TD* RemoveElement(const T& name)
     {
         TD* pData = NULL;
-        typename NFMapOBJECT::iterator itr = mObjectList.find(name);
+        typename map_data::iterator itr = mObjectList.find(name);
         if(itr != mObjectList.end())
         {
             pData = itr->second;
@@ -133,22 +57,9 @@ public:
         return pData;
     }
 
-    /**
-     * @fn  virtual TD* NFMap::GetElement(const T& name)
-     *
-     * @brief   Gets an element.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @param   name    The name.
-     *
-     * @return  Null if it fails, else the element.
-     */
-
     virtual TD* GetElement(const T& name)
     {
-        typename NFMapOBJECT::iterator itr = mObjectList.find(name);
+        typename map_data::iterator itr = mObjectList.find(name);
         if(itr != mObjectList.end())
         {
             return itr->second;
@@ -158,17 +69,6 @@ public:
             return NULL;
         }
     }
-
-    /**
-     * @fn  virtual TD* NFMap::First()
-     *
-     * @brief   Gets the first.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @return  Null if it fails, else a pointer to a TD.
-     */
 
     virtual TD* First()
     {
@@ -188,17 +88,6 @@ public:
         }
     }
 
-    /**
-     * @fn  virtual TD* NFMap::Next()
-     *
-     * @brief   Move to the next item in the collection.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @return  Null if it fails, else a pointer to a TD.
-     */
-
     virtual TD* Next()
     {
         if(mObjectCurIter == mObjectList.end())
@@ -216,19 +105,6 @@ public:
             return NULL;
         }
     }
-
-    /**
-     * @fn  virtual TD* NFMap::First(T& name)
-     *
-     * @brief   Firsts the given name.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @param [in,out]  name    The name.
-     *
-     * @return  Null if it fails, else a pointer to a TD.
-     */
 
     virtual TD* First(T& name)
     {
@@ -249,19 +125,6 @@ public:
         }
     }
 
-    /**
-     * @fn  virtual TD* NFMap::Next(T& name)
-     *
-     * @brief   Move to the next item in the collection.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @param [in,out]  name    The name.
-     *
-     * @return  Null if it fails, else a pointer to a TD.
-     */
-
     virtual TD* Next(T& name)
     {
         if(mObjectCurIter == mObjectList.end())
@@ -275,38 +138,14 @@ public:
             name = mObjectCurIter->first;
             return mObjectCurIter->second;
         }
-        else
-        {
-            return NULL;
-        }
-    }
 
-    /**
-     * @fn  int NFMap::Count()
-     *
-     * @brief   Gets the count.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @return  An int.
-     */
+        return NULL;
+    }
 
     int Count()
     {
         return mObjectList.size();
     }
-
-    /**
-     * @fn  bool NFMap::ClearAll()
-     *
-     * @brief   Clears all.
-     *
-     * @author  flyicegood
-     * @date    2016/11/22
-     *
-     * @return  True if it succeeds, false if it fails.
-     */
 
     bool ClearAll()
     {
@@ -314,20 +153,6 @@ public:
         return true;
     }
 private:
-    /** @brief   List of objects. */
-    NFMapOBJECT     mObjectList;
-
-    /**
-     * @property    typename NFMapOBJECT::iterator mObjectCurIter
-     *
-     * @brief   Gets the object current iterator.
-     *
-     * @return  The m object current iterator.
-     */
-
-    typename NFMapOBJECT::iterator mObjectCurIter;
+    map_data mObjectList;
+    typename map_data::iterator mObjectCurIter;
 };
-
-#endif
-
-
