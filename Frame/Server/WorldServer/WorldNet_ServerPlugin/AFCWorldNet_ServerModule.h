ebaf0d10f8ed13463271ce939a0c6679cd5e91f1
NickYang <362148418@qq.com>
2017-09-25 19:42:39 +0800

    add assert macros replace some AFMapEx with AFArrayMap
    Former-commit-id: 088852c07dbd6116a6be5ef275ff9e0071cb0491
    Former-commit-id: c84a931a26b1d41ff7a51b55d6bcf188186a8771
    Former-commit-id: 3eca5e91cd8710d40732b51d185e58865db1eac1

--- Frame/Server/WorldServer/WorldNet_ServerPlugin/AFCWorldNet_ServerModule.h
+++ Frame/Server/WorldServer/WorldNet_ServerPlugin/AFCWorldNet_ServerModule.h
@@ -1,32 +1,25 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file      AFCWorldNet_ServerModule.h                                                *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCWorldNet_ServerModule                                                  *
-*****************************************************************************/
-#ifndef AFC_WORLDNET_SERVER_MODULE_H
-#define AFC_WORLDNET_SERVER_MODULE_H
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
-//  the cause of sock'libariy, thenfore "AFCNet.h" much be included first.
-#include "SDK/Base/AFMap.h"
+#pragma once
+
 #include "SDK/Proto/AFMsgDefine.h"
 #include "SDK/Interface/AFIWorldToMasterModule.h"
 #include "SDK/Interface/AFIWorldLogicModule.h"
@@ -73,7 +66,6 @@ public:
     virtual int GetPlayerGameID(const AFGUID self);
 
 protected:
-
     void OnSocketEvent(const NetEventType eEvent, const AFGUID& xClientID, const int nServerID);
 
     //连接丢失,删2层(连接对象，帐号对象)
@@ -81,10 +73,7 @@ protected:
     //有连接
     void OnClientConnected(const AFGUID& xClientID);
 
-
-
 protected:
-
     void OnGameServerRegisteredProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
     void OnGameServerUnRegisteredProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
     void OnRefreshGameServerInfoProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
@@ -103,13 +92,11 @@ protected:
     void LogGameServer();
 
 protected:
-
     void OnOnlineProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
     void OnOfflineProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
 
 
 private:
-
     int64_t mnLastCheckTime;
 
     //serverid,data
@@ -122,9 +109,4 @@ private:
     AFIKernelModule* m_pKernelModule;
     AFILogModule* m_pLogModule;
     AFINetServerModule* m_pNetModule;
-
 };
-
-#endif
-
-
