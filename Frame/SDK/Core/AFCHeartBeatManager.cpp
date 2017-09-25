c34c068dfbda0fea0e927e309c4e151c6ee1f937
NickYang <362148418@qq.com>
2017-09-25 20:04:28 +0800

    fix compiling errors
    Former-commit-id: 31f804a0e6afc242c9ac10af4122b98f8ea6e190 [formerly 45f3686eb4acd6a7c8cb7cdeebd0fe18474e1960] [formerly 986556d0009d01673c6705b1e06d60f175646a41] [formerly c1fe1f2c5e473d4abde35f9a09e50dcd618341c0]
    Former-commit-id: d14525a7ccc07d0d6cd2adde7d099947c0933935
    Former-commit-id: 981d48b80be61cf9998387b61e17855ed1326391
    Former-commit-id: 44e57bf597307b874dfd6234fc0d20517d3863e9

--- Frame/SDK/Core/AFCHeartBeatManager.cpp
+++ Frame/SDK/Core/AFCHeartBeatManager.cpp
@@ -1,34 +1,30 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file      AFCHeartBeatManager.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCHeartBeatManager                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "SDK/Base/AFTime.hpp"
 #include "AFCHeartBeatManager.h"
 
 
 AFCHeartBeatManager::~AFCHeartBeatManager()
 {
-    mHeartBeatElementMapEx.ClearAll();
+    mHeartBeatElementMapEx.Clear();
 }
 
 void AFCHeartBeatElement::DoHeartBeatEvent(int64_t nNowTime)
@@ -79,11 +75,16 @@ bool AFCHeartBeatManager::Execute()
 {
     //millisecond
     int64_t nTime = AFCTimeBase::GetInstance().GetNowMillisecond();
-    for(std::multimap<int64_t, ARK_SHARE_PTR<AFCHeartBeatElement>>::iterator iter = mTimeList.begin(); iter != mTimeList.end();)
+    for(std::multimap<int64_t, AFCHeartBeatElement*>::iterator iter = mTimeList.begin(); iter != mTimeList.end();)
     {
         if(iter->second->IsStop())
         {
-            ARK_SHARE_PTR<AFCHeartBeatElement>  pElement = mHeartBeatElementMapEx.GetElement(iter->second->strBeatName);
+            AFCHeartBeatElement* pElement = mHeartBeatElementMapEx.GetElement(iter->second->strBeatName);
+            if (NULL == pElement)
+            {
+                continue;
+            }
+
             if(pElement->id == iter->second->id)
             {
                 mHeartBeatElementMapEx.RemoveElement(iter->second->strBeatName);
@@ -99,7 +100,12 @@ bool AFCHeartBeatManager::Execute()
 
             if(iter->second->IsStop())
             {
-                ARK_SHARE_PTR<AFCHeartBeatElement>  pElement = mHeartBeatElementMapEx.GetElement(iter->second->strBeatName);
+                AFCHeartBeatElement* pElement = mHeartBeatElementMapEx.GetElement(iter->second->strBeatName);
+                if (NULL == pElement)
+                {
+                    continue;
+                }
+
                 if(pElement->id == iter->second->id)
                 {
                     mHeartBeatElementMapEx.RemoveElement(iter->second->strBeatName);
@@ -117,22 +123,21 @@ bool AFCHeartBeatManager::Execute()
         break;
     }
 
-
     //删除所有过时心跳
     std::string strHeartBeatName;
     bool bRet = mRemoveListEx.First(strHeartBeatName);
     while(bRet)
     {
-        ARK_SHARE_PTR<AFCHeartBeatElement> pHeartBeatEx = mHeartBeatElementMapEx.GetElement(strHeartBeatName);
+        AFCHeartBeatElement* pHeartBeatEx = mHeartBeatElementMapEx.GetElement(strHeartBeatName);
         if(NULL == pHeartBeatEx)
         {
             continue;
         }
 
-        typedef std::pair<std::multimap<int64_t, ARK_SHARE_PTR<AFCHeartBeatElement>>::iterator, std::multimap<int64_t, ARK_SHARE_PTR<AFCHeartBeatElement>>::iterator > Range;
+        typedef std::pair<std::multimap<int64_t, AFCHeartBeatElement*>::iterator, std::multimap<int64_t, AFCHeartBeatElement*>::iterator > Range;
         Range xRange = mTimeList.equal_range(pHeartBeatEx->nNextTriggerTime);
 
-        for(std::multimap<int64_t, ARK_SHARE_PTR<AFCHeartBeatElement>>::iterator iter = xRange.first; iter != xRange.second; ++iter)
+        for(std::multimap<int64_t, AFCHeartBeatElement*>::iterator iter = xRange.first; iter != xRange.second; ++iter)
         {
             if(iter->second->id == pHeartBeatEx->id)
             {
@@ -156,7 +161,7 @@ bool AFCHeartBeatManager::Execute()
     {
         if(NULL == mHeartBeatElementMapEx.GetElement(iter->strBeatName))
         {
-            ARK_SHARE_PTR<AFCHeartBeatElement> pHeartBeatEx(ARK_NEW AFCHeartBeatElement());
+            AFCHeartBeatElement* pHeartBeatEx = ARK_NEW AFCHeartBeatElement();
             *pHeartBeatEx = *iter;
             mHeartBeatElementMapEx.AddElement(pHeartBeatEx->strBeatName, pHeartBeatEx);
             mTimeList.insert(std::make_pair(pHeartBeatEx->nNextTriggerTime, pHeartBeatEx));
