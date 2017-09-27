a622faafe8562231badd7f7437314cb29d5f58ea
flyicegood <flyicegood@163.com>
2017-09-27 21:53:36 +0800

    modify some warning
    Former-commit-id: 013b6c98cc4761e27a8e9c4842f61d4349c414c6 [formerly caabe21611d22ab67e5351961cac5ed7e3f839fd] [formerly fc8b2380ac598ee4749a1e9cb6fda350d4bbcae6] [formerly da7f743f0a2a0c0159f59fd1219638218b52ae25]
    Former-commit-id: fee50ab29ae76b4a920ff90c3c784b75f9edd7fc
    Former-commit-id: 03aee23f78fe3916b014c59756eb3a4b5c2b50f7
    Former-commit-id: 677a025181b3daa6cfb76328c08a29d5bb18d14d

--- Frame/SDK/UtilityPlugin/AFCGUIDModule.cpp
+++ Frame/SDK/UtilityPlugin/AFCGUIDModule.cpp
@@ -17,7 +17,7 @@
 // * limitations under the License.                                          *
 // *                                                                         *
 // *                                                                         *
-// * @file  	AFCGUIDModule.cpp                                              *
+// * @file      AFCGUIDModule.cpp                                              *
 // * @author    Ark Game Tech                                                *
 // * @date      2015-12-15                                                   *
 // * @brief     AFCGUIDModule                                                  *
@@ -27,19 +27,19 @@
 
 namespace GUIDModule
 {
-    //https://github.com/nebula-im/snowflake4cxx
-    /**
-    * Twitter_Snowflake
-    * SnowFlake的结构如下(每部分用-分开):
-    * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
-    * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
-    * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
-    * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
-    * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
-    * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
-    * 加起来刚好64位。
-    * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高。
-    */
+//https://github.com/nebula-im/snowflake4cxx
+/**
+* Twitter_Snowflake
+* SnowFlake的结构如下(每部分用-分开):
+* 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+* 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
+* 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+* 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+* 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
+* 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
+* 加起来刚好64位。
+* SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高。
+*/
 
 #ifndef _MSC_VER
 # include <sys/time.h>
@@ -50,100 +50,100 @@ namespace GUIDModule
 #endif
 
 #ifdef _MSC_VER
-    int gettimeofday(struct timeval* tp, void *tzp)
-    {
-        time_t clock;
-        struct tm tm;
-        SYSTEMTIME wtm;
-        GetLocalTime(&wtm);
-        tm.tm_year = wtm.wYear - 1900;
-        tm.tm_mon = wtm.wMonth - 1;
-        tm.tm_mday = wtm.wDay;
-        tm.tm_hour = wtm.wHour;
-        tm.tm_min = wtm.wMinute;
-        tm.tm_sec = wtm.wSecond;
-        tm.tm_isdst = -1;
-        clock = mktime(&tm);
-        tp->tv_sec = clock;
-        tp->tv_usec = wtm.wMilliseconds * 1000;
-        return (0);
-    }
+int gettimeofday(struct timeval* tp, void *tzp)
+{
+    time_t clock;
+    struct tm tm;
+    SYSTEMTIME wtm;
+    GetLocalTime(&wtm);
+    tm.tm_year = wtm.wYear - 1900;
+    tm.tm_mon = wtm.wMonth - 1;
+    tm.tm_mday = wtm.wDay;
+    tm.tm_hour = wtm.wHour;
+    tm.tm_min = wtm.wMinute;
+    tm.tm_sec = wtm.wSecond;
+    tm.tm_isdst = -1;
+    clock = mktime(&tm);
+    tp->tv_sec = clock;
+    tp->tv_usec = wtm.wMilliseconds * 1000;
+    return (0);
+}
 #endif
 
-    uint64_t GetNowInMsec()
-    {
-        struct timeval tv;
-        gettimeofday(&tv, 0);
-        return uint64_t(tv.tv_sec) * 1000 + tv.tv_usec / 1000;
-    }
+uint64_t GetNowInMsec()
+{
+    struct timeval tv;
+    int  nRet = gettimeofday(&tv, 0);
+    return uint64_t(tv.tv_sec) * 1000 + tv.tv_usec / 1000;
+}
 
-    uint64_t WaitUntilNextMillis(uint64_t last_timestamp)
+uint64_t WaitUntilNextMillis(uint64_t last_timestamp)
+{
+    uint64_t timestamp = GetNowInMsec();
+    while(timestamp <= last_timestamp)
+    {
+        timestamp = GetNowInMsec();
+    }
+    return timestamp;
+}
+
+class IdWorkerUnThreadSafe
+{
+public:
+    IdWorkerUnThreadSafe(uint16_t worker_id, uint16_t data_center_id)
+        : worker_id_(worker_id)
+        , data_center_id_(data_center_id)
+    {}
+
+    uint64_t GetNextID()
     {
         uint64_t timestamp = GetNowInMsec();
-        while (timestamp <= last_timestamp)
+
+        // 鍦ㄥ綋鍓嶇鍐?
+        if(last_timestamp_ == timestamp)
         {
-            timestamp = GetNowInMsec();
+            sequence_ = (sequence_ + 1) & 0xFFF;
+            if(sequence_ == 0)
+            {
+                timestamp = WaitUntilNextMillis(last_timestamp_);
+            }
         }
-        return timestamp;
-    }
-
-    class IdWorkerUnThreadSafe
-    {
-    public:
-        IdWorkerUnThreadSafe(uint16_t worker_id, uint16_t data_center_id)
-            : worker_id_(worker_id)
-            , data_center_id_(data_center_id)
-        {}
-
-        uint64_t GetNextID()
+        else
         {
-            uint64_t timestamp = GetNowInMsec();
+            sequence_ = 0;
+        }
 
-            // 鍦ㄥ綋鍓嶇鍐?
-            if (last_timestamp_ == timestamp)
-            {
-                sequence_ = (sequence_ + 1) & 0xFFF;
-                if (sequence_ == 0)
-                {
-                    timestamp = WaitUntilNextMillis(last_timestamp_);
-                }
-            }
-            else
-            {
-                sequence_ = 0;
-            }
-
-            last_timestamp_ = timestamp;
-            return ((timestamp & 0x1FFFFFF) << 22 |
+        last_timestamp_ = timestamp;
+        return ((timestamp & 0x1FFFFFF) << 22 |
                 (data_center_id_ & 0x1F) << 17 |
                 (worker_id_ & 0x1F) << 12 |
                 (sequence_ & 0xFFF));
-        }
+    }
 
-    protected:
-        uint16_t worker_id_{ 0 };
-        uint16_t data_center_id_{ 0 };
-        uint64_t last_timestamp_{ 0 };
-        uint32_t sequence_{ 0 };
-    };
+protected:
+    uint16_t worker_id_{ 0 };
+    uint16_t data_center_id_{ 0 };
+    uint64_t last_timestamp_{ 0 };
+    uint32_t sequence_{ 0 };
+};
 
-    class IdWorkerThreadSafe
+class IdWorkerThreadSafe
+{
+public:
+    IdWorkerThreadSafe(uint16_t worker_id, uint16_t data_center_id)
+        : id_worker_(worker_id, data_center_id)
+    {}
+
+    uint64_t GetNextID()
     {
-    public:
-        IdWorkerThreadSafe(uint16_t worker_id, uint16_t data_center_id)
-            : id_worker_(worker_id, data_center_id)
-        {}
+        std::lock_guard<std::mutex> g(lock_);
+        return id_worker_.GetNextID();
+    }
 
-        uint64_t GetNextID()
-        {
-            std::lock_guard<std::mutex> g(lock_);
-            return id_worker_.GetNextID();
-        }
-
-    protected:
-        IdWorkerUnThreadSafe id_worker_;
-        std::mutex lock_;
-    };
+protected:
+    IdWorkerUnThreadSafe id_worker_;
+    std::mutex lock_;
+};
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -170,7 +170,7 @@ bool AFCGUIDModule::Execute()
 
 bool AFCGUIDModule::BeforeShut()
 {
-    if (NULL != m_pIDWoker)
+    if(NULL != m_pIDWoker)
     {
         delete m_pIDWoker;
         m_pIDWoker = NULL;
