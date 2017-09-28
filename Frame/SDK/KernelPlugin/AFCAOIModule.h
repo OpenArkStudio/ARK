1c493a971e9f67ccd44af399e2a5c33ca41fef69
NickYang <362148418@qq.com>
2017-09-28 20:14:31 +0800

    add more CMakeLists.txt (not finished)
    Former-commit-id: db7d5cbc1fc48f785a598825a4e40210a56f86df [formerly 821a6db1127f5a1b889463ef1e0030913fb2c597] [formerly 1dc67a80a1d2ce490cb8e58925dd8f1bc7561e2c] [formerly 578ba363aae8bf2e7ad7177b28110c9581f4bffb]
    Former-commit-id: a641e179a3047518ebd8a6e3a49b10b77b17a2c6
    Former-commit-id: fef0216e41afe4c9060241cff6681a8db313f760
    Former-commit-id: c92a7668362907715d4e98d6848349a01f03cc0f

--- Frame/SDK/KernelPlugin/AFCAOIModule.h
+++ Frame/SDK/KernelPlugin/AFCAOIModule.h
@@ -1,35 +1,30 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFCAOIModule.h                                                *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCAOIModule                                                  *
-*****************************************************************************/
-#ifndef NF_AOI_MODULE_H
-#define NF_AOI_MODULE_H
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
 #pragma once
-#include "SDK/Core/NFGUID.h"
-#include "SDK/Core/AFIDataList.h"
-#include <map>
-#include <list>
+
+#include "SDK/Base/AFGUID.h"
+#include "SDK/Base/AFIDataList.h"
+#include "SDK/Base/AFIData.h"
+
+using namespace ArkFrame;
 
 #define DEFAULT_RADIUS 20
 
@@ -39,13 +34,13 @@ public:
     AFCAOIModule();
     ~AFCAOIModule();
 
-    void Add(const NFGUID& self, const Point3D& xPos, int nRadius = DEFAULT_RADIUS);
-    void Move(const NFGUID& self, const Point3D& xPos);
-    void Leave(const NFGUID& self);
+    void Add(const AFGUID& self, const Point3D& xPos, int nRadius = DEFAULT_RADIUS);
+    void Move(const AFGUID& self, const Point3D& xPos);
+    void Leave(const AFGUID& self);
 
     struct AOIObject
     {
-        AOIObject(const NFGUID& self, const Point3D& pos, const int nRadius)
+        AOIObject(const AFGUID& self, const Point3D& pos, const int nRadius)
         {
             id = self;
             x = (int)pos.x;
@@ -55,14 +50,14 @@ public:
 
         int x = 0;
         int y = 0;
-        NFGUID id = NULL_GUID;
+        AFGUID id = NULL_GUID;
         int radius = 0;
         std::list<AOIObject*>::iterator x_pos;
         std::list<AOIObject*>::iterator y_pos;
     };
 
 private:
-    using ObjectMap = std::map<NFGUID, AOIObject*>;
+    using ObjectMap = std::map<AFGUID, AOIObject*>;
     using ObjectList = std::list<AOIObject*>;
 
     void Update(AOIObject* pObject);
@@ -78,6 +73,3 @@ private:
     ObjectMap enterMap;
     ObjectMap leaveMap;
 };
-
-#endif //!NF_AOI_MODULE_H
-
