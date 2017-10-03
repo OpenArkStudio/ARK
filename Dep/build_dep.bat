42e8f15028e406634a563941dfb992c538e91110
NickYang <362148418@qq.com>
2017-10-03 15:07:28 +0800

    new build_dep.bat for Windows
    Former-commit-id: bc494070993a992b26bb562933c4a1bc036dd63a [formerly 0c53de9e0d60cefc5805122d9dc59130e06a6b9f] [formerly cac7123cbc97e8bea7a53792d15665b3b4d68c39] [formerly 81c2f354b5eb4c8dd831b4ebef4331a6872376f3]
    Former-commit-id: 1a24e1c7b0624105c30c596d1fce590a0b1e4db1
    Former-commit-id: fed0466d45f1593e05d76f01817fbfe3abf3ac03
    Former-commit-id: 7a072f3e7d0e7ee24d3c92725dad40e2dfbf50f5

--- Dep/build_dep.bat
+++ Dep/build_dep.bat
@@ -1,25 +1,63 @@
 @echo off
 
-echo Building Dep...
+echo Building dependencies...
 
-echo Compiling libevent
-"..\\Tool\\7z.exe" x libevent-release-2.1.8-stable.zip -Y 
+if exist lib (rd lib /f /q /s)
+md lib
+REM ######################################################################################################
+echo Building protobuf...
 
-rd libevent  /q /s
-rename libevent-release-2.1.8-stable libevent
+if exist protobuf (rd protobuf /q /s)
+git clone -b 2.7.0 https://github.com/google/protobuf.git
 
-echo Compiling evpp
-"..\\Tool\\7z.exe" x evpp.zip -Y -o./evpp
+cd protobuf/cmake
+md build
+cd build
+cmake -G "Visual Studio 14 Win64" -Dprotobuf_BUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF ..
+"%VS140COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Release|x64"
+copy Release\*.dll ..\..\..\lib /Y
+cd ..\..\..\
+REM ######################################################################################################
+echo Building libevent...
 
-echo Compiling glog
-"..\\Tool\\7z.exe" x glog-0.3.5.zip -Y 
+if exist libevent (rd libevent /q /s)
+git clone -b master https://github.com/libevent/libevent.git
 
-rd glog  /q /s
-rename glog-0.3.5 glog
+cd libevent
+md build
+cd build
+cmake -G "Visual Studio 14 Win64" -DEVENT__DISABLE_OPENSSL=ON ..
+"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64"
+copy lib\Release\*.lib ..\..\lib /Y
+cd ..\..\
+REM ######################################################################################################
+echo Building glog...
+if exist glog (rd glog /q /s)
+git clone -b v035 https://github.com/google/glog.git
 
+cd glog
+md build
+cd build
+cmake -G "Visual Studio 14 Win64" ..
+"%VS140COMNTOOLS%..\IDE\Devenv" google-glog.sln /build "Release|x64"
+copy Release\*.lib ..\..\lib /Y
+cd ..\..\
+REM ######################################################################################################
+echo Building evpp...
 
-echo extra lib
-"..\\Tool\\7z.exe" x lib.zip -Y 
+if exist evpp (rd evpp /q /s)
+git clone -b master https://github.com/Qihoo360/evpp.git
 
+cd evpp
+md build
+cd build
+git clone https://github.com/Microsoft/vcpkg.git 
+cd vcpkg
+call bootstrap-vcpkg.bat
+vcpkg install evpp:x64-windows
 
-
+copy packages\evpp_x64-windows\lib\evpp*.lib ..\..\..\lib /Y
+cd ..\..\..\
+REM ####################################################################################################
+REM back to root dir
+cd ../
