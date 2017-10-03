65ff34cd0a2a0e5b861351cd3d82f3a784fd8720
NickYang <362148418@qq.com>
2017-10-03 18:51:40 +0800

    restructure the libs
    Former-commit-id: c49f05b56a704400b756d684aac8b90094be6064 [formerly cbe592ebf2fecf606232a59369f99762c62cc57d] [formerly 54bd7611428ee9343711a341d9cb65d903d64aff] [formerly ef085dadfdf424771cd458a27ab4876986c7577c]
    Former-commit-id: b531572338989a296fd9b78bf7936748aa8f6766
    Former-commit-id: 5e1e3aede315ad10f13863d18656421a551b938e
    Former-commit-id: 717f51bef9d345d6d24439a8aaec498eb88b9aa3

--- Frame/SDK/Proto/gen-proto.bat
+++ Frame/SDK/Proto/gen-proto.bat
@@ -8,12 +8,12 @@
 "proto-gen/protoc.exe" -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ ./AFMsgURl.proto 
 "proto-gen/protoc.exe" -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ ./AFMsgMysql.proto
 
-"proto-gen/protogen.exe" -i:AFDefine.proto -o:AFDefine.cs
-"proto-gen/protogen.exe" -i:AFMsgBase.proto -o:AFMsgBase.cs
-"proto-gen/protogen.exe" -i:AFMsgPreGame.proto -o:AFMsgPreGame.cs
-"proto-gen/protogen.exe" -i:AFMsgShare.proto -o:AFMsgShare.cs
-"proto-gen/protogen.exe" -i:AFSLGDefine.proto -o:AFSLGDefine.cs
-"proto-gen/protogen.exe" -i:AFFleetingDefine.proto -o:AFFleetingDefine.cs
-"proto-gen/protogen.exe" -i:AFMsgURl.proto -o:AFMsgURl.cs
+"proto-gen/protogen.exe" -i:AFDefine.proto -o:cs/AFDefine.cs
+"proto-gen/protogen.exe" -i:AFMsgBase.proto -o:cs/AFMsgBase.cs
+"proto-gen/protogen.exe" -i:AFMsgPreGame.proto -o:cs/AFMsgPreGame.cs
+"proto-gen/protogen.exe" -i:AFMsgShare.proto -o:cs/AFMsgShare.cs
+"proto-gen/protogen.exe" -i:AFSLGDefine.proto -o:cs/AFSLGDefine.cs
+"proto-gen/protogen.exe" -i:AFFleetingDefine.proto -o:cs/AFFleetingDefine.cs
+"proto-gen/protogen.exe" -i:AFMsgURl.proto -o:cs/AFMsgURl.cs
 
 pause
