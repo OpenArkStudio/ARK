8ac92b00d1fdcb4a7596428f8fd6586725d5bf19
NickYang <362148418@qq.com>
2017-10-06 18:22:49 +0800

    unify the comment of every files
    Former-commit-id: 038a2c3a368c2447d6ec0f43e70cf57ad53ec700 [formerly d6e136820e4bb7ee39634be76401967fe43b11ba] [formerly 090738f7aa8219a9da4643443a9c6d44e6673a69] [formerly ef20fb09d702039bbbb081b9bf87535cd4ee6b65]
    Former-commit-id: 63c97880f72d0b7f9ee6258f726e865aeb5bc60b
    Former-commit-id: febb55df3fc004be210c9a53e03ab1b21e3b3a7b
    Former-commit-id: cfcfecee9c11ce30b30ad90abdfc5fb16c9f9ed0

--- Frame/Server/LoginServer/LoginLogicPlugin/AFLoginLogicPlugin.cpp
+++ Frame/Server/LoginServer/LoginLogicPlugin/AFLoginLogicPlugin.cpp
@@ -1,37 +1,32 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFLoginLogicPlugin.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFLoginLogicPlugin                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "AFLoginLogicPlugin.h"
 #include "AFCLoginLogicModule.h"
 
-//
 #ifdef ARK_DYNAMIC_PLUGIN
 
 ARK_EXPORT void DllStartPlugin(AFIPluginManager* pm)
 {
 #if ARK_PLATFORM == PLATFORM_WIN
-    SetConsoleTitle("NFLoginServer");
+    SetConsoleTitle("LoginServer -- ArkGame");
 #endif
     CREATE_PLUGIN(pm, AFLoginLogicPlugin)
 
