3d8eed6c300ff8d1a2d1a04e38be48c17bfff5ed
NickYang <362148418@qq.com>
2017-10-06 15:34:42 +0800

    restructure the directories
    Former-commit-id: 2244b5c243ac777939d5363085ece9a10a765495 [formerly 3b900c0eb74fb3f77109e149a63a4981c3417cd3] [formerly fe1e246655900fdf0f9236fde954f6bcde1c5135 [formerly 9ca647014bbb4f2b80d5d33d8eeaef6b9c369ad9]]
    Former-commit-id: b041780c7dfd963e99beb5112efd8407c7975393 [formerly ddac454d1b08f7271d94aac62b1d8370cea5ebe3]
    Former-commit-id: 5ffbd9ecf40cd3cb3caf42009ebc3a3deba70c77
    Former-commit-id: 1ea668f494682c73444257d5a39e4115f10b2c31

---
+++ Frame/Server/ProxyServer/ProxyNetClientPlugin/AFCProxyServerToGameModule.cpp
@@ -0,0 +1,207 @@
+/*****************************************************************************
+// * This source file is part of ArkGameFrame                                *
+// * For the latest info, see https://github.com/ArkGame                     *
+// *                                                                         *
+// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
+// *                                                                         *
+// * Licensed under the Apache License, Version 2.0 (the "License");         *
+// * you may not use this file except in compliance with the License.        *
+// * You may obtain a copy of the License at                                 *
+// *                                                                         *
+// *     http://www.apache.org/licenses/LICENSE-2.0                          *
+// *                                                                         *
+// * Unless required by applicable law or agreed to in writing, software     *
+// * distributed under the License is distributed on an "AS IS" BASIS,       *
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+// * See the License for the specific language governing permissions and     *
+// * limitations under the License.                                          *
+// *                                                                         *
+// *                                                                         *
+// * @file      AFCProxyServerToGameModule.cpp                                              *
+// * @author    Ark Game Tech                                                *
+// * @date      2015-12-15                                                   *
+// * @brief     AFCProxyServerToGameModule                                                  *
+*****************************************************************************/
+#include "AFCProxyServerToGameModule.h"
+#include "AFProxyNetClientPlugin.h"
+#include "SDK/Core/AFIHeartBeatManager.h"
+#include "SDK/Core/AFCHeartBeatManager.h"
+#include "SDK/Interface/AFIClassModule.h"
+
+bool AFCProxyServerToGameModule::Init()
+{
+    m_pNetClientModule = ARK_NEW AFINetClientModule(pPluginManager);
+
+    m_pNetClientModule->Init();
+
+    return true;
+}
+
+bool AFCProxyServerToGameModule::Shut()
+{
+    //Final();
+    //Clear();
+    return true;
+}
+
+bool AFCProxyServerToGameModule::Execute()
+{
+    m_pNetClientModule->Execute();
+
+    return true;
+}
+
+bool AFCProxyServerToGameModule::AfterInit()
+{
+    m_pProxyLogicModule = pPluginManager->FindModule<AFIProxyLogicModule>();
+    m_pKernelModule = pPluginManager->FindModule<AFIKernelModule>();
+    m_pProxyServerNet_ServerModule = pPluginManager->FindModule<AFIProxyNetServerModule>();
+    m_pElementModule = pPluginManager->FindModule<AFIElementModule>();
+    m_pLogModule = pPluginManager->FindModule<AFILogModule>();
+    m_pClassModule = pPluginManager->FindModule<AFIClassModule>();
+
+    m_pNetClientModule->AddReceiveCallBack(AFMsg::EGMI_ACK_ENTER_GAME, this, &AFCProxyServerToGameModule::OnAckEnterGame);
+    m_pNetClientModule->AddReceiveCallBack(AFMsg::EGMI_GTG_BROCASTMSG, this, &AFCProxyServerToGameModule::OnBrocastmsg);
+    m_pNetClientModule->AddReceiveCallBack(this, &AFCProxyServerToGameModule::Transpond);
+
+    m_pNetClientModule->AddEventCallBack(this, &AFCProxyServerToGameModule::OnSocketGSEvent);
+
+    ARK_SHARE_PTR<AFIClass> xLogicClass = m_pClassModule->GetElement("Server");
+    if(nullptr != xLogicClass)
+    {
+        NFList<std::string>& xNameList = xLogicClass->GetConfigNameList();
+        std::string strConfigName;
+        for(bool bRet = xNameList.First(strConfigName); bRet; bRet = xNameList.Next(strConfigName))
+        {
+            const int nServerType = m_pElementModule->GetPropertyInt(strConfigName, "Type");
+            const int nServerID = m_pElementModule->GetPropertyInt(strConfigName, "ServerID");
+            if(nServerType == NF_SERVER_TYPES::NF_ST_GAME)
+            {
+                const int nPort = m_pElementModule->GetPropertyInt(strConfigName, "Port");
+                const int nMaxConnect = m_pElementModule->GetPropertyInt(strConfigName, "MaxOnline");
+                const int nCpus = m_pElementModule->GetPropertyInt(strConfigName, "CpuCount");
+                const std::string strName(m_pElementModule->GetPropertyString(strConfigName, "Name"));
+                const std::string strIP(m_pElementModule->GetPropertyString(strConfigName, "IP"));
+
+                ConnectData xServerData;
+
+                xServerData.nGameID = nServerID;
+                xServerData.eServerType = (NF_SERVER_TYPES)nServerType;
+                xServerData.strIP = strIP;
+                xServerData.nPort = nPort;
+                xServerData.strName = strName;
+
+                m_pNetClientModule->AddServer(xServerData);
+            }
+        }
+    }
+
+    return true;
+}
+
+AFINetClientModule* AFCProxyServerToGameModule::GetClusterModule()
+{
+    return m_pNetClientModule;
+}
+
+void AFCProxyServerToGameModule::OnSocketGSEvent(const NetEventType eEvent, const AFGUID& xClientID, const int nServerID)
+{
+    if(eEvent == DISCONNECTED)
+    {
+    }
+    else  if(eEvent == CONNECTED)
+    {
+        m_pLogModule->LogInfo(xClientID, "NF_NET_EVENT_CONNECTED", "connectioned success", __FUNCTION__, __LINE__);
+        Register(nServerID);
+    }
+}
+
+void AFCProxyServerToGameModule::Register(const int nServerID)
+{
+    ARK_SHARE_PTR<AFIClass> xLogicClass = m_pClassModule->GetElement("Server");
+    if(nullptr != xLogicClass)
+    {
+        NFList<std::string>& xNameList = xLogicClass->GetConfigNameList();
+        std::string strConfigName;
+        for(bool bRet = xNameList.First(strConfigName); bRet; bRet = xNameList.Next(strConfigName))
+        {
+            const int nServerType = m_pElementModule->GetPropertyInt(strConfigName, "Type");
+            const int nSelfServerID = m_pElementModule->GetPropertyInt(strConfigName, "ServerID");
+            if(nServerType == NF_SERVER_TYPES::NF_ST_PROXY && pPluginManager->AppID() == nSelfServerID)
+            {
+                const int nPort = m_pElementModule->GetPropertyInt(strConfigName, "Port");
+                const int nMaxConnect = m_pElementModule->GetPropertyInt(strConfigName, "MaxOnline");
+                const int nCpus = m_pElementModule->GetPropertyInt(strConfigName, "CpuCount");
+                const std::string strName(m_pElementModule->GetPropertyString(strConfigName, "Name"));
+                const std::string strIP(m_pElementModule->GetPropertyString(strConfigName, "IP"));
+
+                AFMsg::ServerInfoReportList xMsg;
+                AFMsg::ServerInfoReport* pData = xMsg.add_server_list();
+
+                pData->set_server_id(nSelfServerID);
+                pData->set_server_name(strName);
+                pData->set_server_cur_count(0);
+                pData->set_server_ip(strIP);
+                pData->set_server_port(nPort);
+                pData->set_server_max_online(nMaxConnect);
+                pData->set_server_state(AFMsg::EST_NARMAL);
+                pData->set_server_type(nServerType);
+
+                ARK_SHARE_PTR<ConnectData> pServerData = m_pNetClientModule->GetServerNetInfo(nServerID);
+                if(pServerData)
+                {
+                    int nTargetID = pServerData->nGameID;
+                    m_pNetClientModule->SendToServerByPB(nTargetID, AFMsg::EGameMsgID::EGMI_PTWG_PROXY_REGISTERED, xMsg, 0);
+
+                    m_pLogModule->LogInfo(AFGUID(0, pData->server_id()), pData->server_name(), "Register");
+                }
+            }
+        }
+    }
+}
+
+void AFCProxyServerToGameModule::OnAckEnterGame(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID)
+{
+    AFGUID nPlayerID;
+    AFMsg::AckEventResult xData;
+    if(!AFINetServerModule::ReceivePB(xHead, nMsgID, msg, nLen, xData, nPlayerID))
+    {
+        return;
+    }
+
+    if(xData.event_code() == AFMsg::EGEC_ENTER_GAME_SUCCESS)
+    {
+        const AFGUID& xClient = AFINetServerModule::PBToNF(xData.event_client());
+        const AFGUID& xPlayer = AFINetServerModule::PBToNF(xData.event_object());
+
+        m_pProxyServerNet_ServerModule->EnterGameSuccessEvent(xClient, xPlayer);
+    }
+}
+
+void AFCProxyServerToGameModule::OnBrocastmsg(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID)
+{
+    //保持记录,直到下线,或者1分钟不上线即可删除
+    AFGUID nPlayerID;
+    AFMsg::BrocastMsg xMsg;
+    if(!AFINetServerModule::ReceivePB(xHead, nMsgID, msg, nLen, xMsg, nPlayerID))
+    {
+        return;
+    }
+
+    for(int i = 0; i < xMsg.player_client_list_size(); i++)
+    {
+        const AFMsg::Ident& xClientID = xMsg.player_client_list(i);
+        m_pProxyServerNet_ServerModule->SendToPlayerClient(xMsg.nmsgid(), xMsg.msg_data().c_str(), xMsg.msg_data().size(), AFINetServerModule::PBToNF(xClientID), nPlayerID);
+    }
+}
+
+void AFCProxyServerToGameModule::LogServerInfo(const std::string& strServerInfo)
+{
+    m_pLogModule->LogInfo(AFGUID(), strServerInfo, "");
+}
+
+void AFCProxyServerToGameModule::Transpond(const AFIMsgHead& xHead, const int nMsgID, const char * msg, const uint32_t nLen, const AFGUID& xClientID)
+{
+    m_pProxyServerNet_ServerModule->Transpond(xHead, nMsgID, msg, nLen);
+}
+
