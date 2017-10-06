4b92653bb6c3de73f15ce90e2600f64b84cec1f5
NickYang <362148418@qq.com>
2017-10-06 15:34:42 +0800

    restructure the directories
    Former-commit-id: 41f919b2ca35e4942d8cf4abc07f06f86e00f777 [formerly cb58ebd4afe4204e10a102396d4806328bb4910d] [formerly 64c85fb38f8202751cafa32f0f8adab22fdb0c8d] [formerly 5e7c8ba3635642cb9a2a7d480a6f9300e2498bee]
    Former-commit-id: dfc3beabb97f1dd3914f59e249ab1893afb9189c
    Former-commit-id: 2ecad1b9b121eff2f67ed831b0ac4399665d253c
    Former-commit-id: 5eaaebb03861a76778b344b3f93c1442020e972d

---
+++ Frame/Server/WorldServer/WorldNetClientPlugin/AFCWorldToMasterModule.h
@@ -0,0 +1,75 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#pragma once
+
+#include "SDK/Proto/AFMsgDefine.h"
+#include "SDK/Interface/AFIWorldToMasterModule.h"
+#include "SDK/Interface/AFIWorldLogicModule.h"
+#include "SDK/Interface/AFINetServerModule.h"
+#include "SDK/Interface/AFIClassModule.h"
+#include "SDK/Interface/AFIElementModule.h"
+#include "SDK/Interface/AFILogModule.h"
+#include "SDK/Interface/AFIWorldNetServerModule.h"
+#include "SDK/Interface/AFINetClientModule.hpp"
+
+class AFCWorldToMasterModule
+    : public AFIWorldToMasterModule
+{
+public:
+    AFCWorldToMasterModule(AFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+
+    virtual bool Init();
+    virtual bool BeforeShut();
+    virtual bool Shut();
+    virtual bool Execute();
+    virtual bool AfterInit();
+
+protected:
+
+    void OnSocketMSEvent(const NetEventType eEvent, const AFGUID& xClientID, const int nServerID);
+
+    //连接丢失,删2层(连接对象，帐号对象)
+    void OnClientDisconnect(const AFGUID& xClientID);
+    //有连接
+    void OnClientConnected(const AFGUID& xClientID);
+
+    virtual void LogServerInfo(const std::string& strServerInfo);
+
+
+    void Register(const int nServerID);
+    void RefreshWorldInfo();
+
+    void OnSelectServerProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
+    void OnKickClientProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
+
+    void InvalidMessage(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
+private:
+
+    AFILogModule* m_pLogModule;
+    AFIElementModule* m_pElementModule;
+    AFIClassModule* m_pClassModule;
+    AFIWorldLogicModule* m_pWorldLogicModule;
+    AFIWorldNetServerModule* m_pWorldNet_ServerModule;
+    AFINetClientModule* m_pNetClientModule;
+};
