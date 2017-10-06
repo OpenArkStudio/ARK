0be7243f8c5a9b5e33a36c68c6c7c17e7a6a62a4
NickYang <362148418@qq.com>
2017-10-06 18:22:49 +0800

    unify the comment of every files
    Former-commit-id: 8b4c4a4a6e69b6686e5aa1404708a67709573fcb

--- Frame/Examples/Tutorial3/Tutorial3Plugin.cpp
+++ Frame/Examples/Tutorial3/Tutorial3Plugin.cpp
@@ -1,27 +1,23 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	Tutorial3Plugin.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     Tutorial3Plugin                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "Tutorial3Plugin.h"
 #include "HelloWorld3Module.h"
 
@@ -33,13 +29,12 @@ ARK_EXPORT void DllStartPlugin(AFIPluginManager* pm)
     SetConsoleTitle("Tutorial3");
 #endif
     CREATE_PLUGIN(pm, Tutorial3Plugin)
-
-};
+}
 
 ARK_EXPORT void DllStopPlugin(AFIPluginManager* pm)
 {
     DESTROY_PLUGIN(pm, Tutorial3Plugin)
-};
+}
 
 #endif
 //////////////////////////////////////////////////////////////////////////
@@ -56,13 +51,10 @@ const std::string Tutorial3Plugin::GetPluginName()
 
 void Tutorial3Plugin::Install()
 {
-
     REGISTER_MODULE(pPluginManager, HelloWorld3Module, HelloWorld3Module)
-
 }
 
 void Tutorial3Plugin::Uninstall()
 {
     UNREGISTER_MODULE(pPluginManager, HelloWorld3Module, HelloWorld3Module)
 }
-
