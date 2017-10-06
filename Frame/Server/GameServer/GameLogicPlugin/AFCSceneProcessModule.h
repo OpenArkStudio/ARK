16eb6be4d1d15d67f71c1c302847e190427f8f15
NickYang <362148418@qq.com>
2017-10-06 15:34:42 +0800

    restructure the directories
    Former-commit-id: ec22e16d25f76e75fcba7d9ff6e5646aba9fa895 [formerly 5e7c8ba3635642cb9a2a7d480a6f9300e2498bee]
    Former-commit-id: dfc3beabb97f1dd3914f59e249ab1893afb9189c

---
+++ Frame/Server/GameServer/GameLogicPlugin/AFCSceneProcessModule.h
@@ -0,0 +1,96 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#pragma once
+
+#include "SDK/Base/AFList.h"
+#include "RapidXML/rapidxml.hpp"
+#include "RapidXML/rapidxml_iterators.hpp"
+#include "RapidXML/rapidxml_print.hpp"
+#include "RapidXML/rapidxml_utils.hpp"
+#include "SDK/Interface/AFIKernelModule.h"
+#include "SDK/Interface/AFIGameLogicModule.h"
+#include "SDK/Interface/AFIElementModule.h"
+#include "SDK/Interface/AFIClassModule.h"
+#include "SDK/Interface/AFIGameServerConfigModule.h"
+#include "SDK/Interface/AFISceneProcessModule.h"
+#include "SDK/Interface/AFIPropertyModule.h"
+#include "SDK/Interface/AFILogModule.h"
+#include "SDK/Interface/AFIPluginManager.h"
+#include "SDK/Proto/NFProtocolDefine.hpp"
+#include "SDK/Interface/AFIGameNetServerModule.h"
+
+class AFCSceneProcessModule
+    : public AFISceneProcessModule
+{
+public:
+    AFCSceneProcessModule(AFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+    virtual ~AFCSceneProcessModule() {};
+
+    virtual bool Init();
+    virtual bool Shut();
+    virtual bool Execute();
+    virtual bool AfterInit();
+
+    virtual E_SCENE_TYPE GetCloneSceneType(const int nSceneID);
+    virtual bool IsCloneScene(const int nSceneID);
+    virtual bool ApplyCloneGroup(const int nSceneID, int& nGroupID);
+    virtual bool ExitCloneGroup(const int nSceneID, const int& nGroupID);
+
+protected:
+    int CreateCloneScene(const int& nSceneID);
+
+    bool CreateSceneObject(const int nSceneID, const int nGroupID);
+
+    bool LoadSceneResource(const int nSceneID);
+
+protected:
+
+    int OnObjectClassEvent(const AFGUID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const AFIDataList& var);
+
+    int OnEnterSceneEvent(const AFGUID& object, const int nEventID, const AFIDataList& var);
+    int OnLeaveSceneEvent(const AFGUID& object, const int nEventID, const AFIDataList& var);
+
+protected:
+    void OnClienSwapSceneProcess(const AFIMsgHead& xHead, const int nMsgID, const char* msg, const uint32_t nLen, const AFGUID& xClientID);
+
+private:
+
+    AFIElementModule* m_pElementModule;
+    AFIClassModule* m_pClassModule;
+    AFIKernelModule* m_pKernelModule;
+    AFILogModule* m_pLogModule;
+    AFIGameNetServerModule* m_pGameServerNet_ServerModule;
+    //////////////////////////////////////////////////////////////////////////
+    struct SceneSeedResource
+    {
+        std::string strSeedID;
+        std::string strConfigID;
+        float fSeedX;
+        float fSeedY;
+        float fSeedZ;
+    };
+
+    //SceneID,(SeedID,SeedData)
+    AFMapEx<int, AFMapEx<std::string, SceneSeedResource>> mtSceneResourceConfig;
+};
--- Frame/Server/LoginServer/LoginLogicPlugin/AFCLoginLogicModule.cpp
+++ Frame/Server/LoginServer/LoginLogicPlugin/AFCLoginLogicModule.cpp
@@ -1,34 +1,28 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFCLoginLogicModule.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCLoginLogicModule                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "AFLoginLogicPlugin.h"
 #include "AFCLoginLogicModule.h"
 
-
 bool AFCLoginLogicModule::Init()
 {
-
     return true;
 }
 
@@ -44,16 +38,12 @@ int AFCLoginLogicModule::OnLoginProcess(const AFGUID& object, const std::string&
 
 bool AFCLoginLogicModule::Execute()
 {
-
-
     return true;
 }
-
 
 bool AFCLoginLogicModule::AfterInit()
 {
-    m_pLoginNet_ServerModule = pPluginManager->FindModule<AFILoginNet_ServerModule>();
+    m_pLoginNet_ServerModule = pPluginManager->FindModule<AFILoginNetServerModule>();
 
     return true;
 }
-
