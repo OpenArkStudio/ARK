7b2e1869a33db02ae57f71a0872b0428a0204319
NickYang <362148418@qq.com>
2017-10-06 18:22:49 +0800

    unify the comment of every files
    Former-commit-id: 8b4c4a4a6e69b6686e5aa1404708a67709573fcb
    Former-commit-id: b292468a1391c5140da03f12fdbb2be26bd02e97
    Former-commit-id: fc32333cb731ed9756307a1eabdadb4a768d68a1

--- Frame/Server/WorldServer/WorldLogicPlugin/AFCWorldLogicModule.cpp
+++ Frame/Server/WorldServer/WorldLogicPlugin/AFCWorldLogicModule.cpp
@@ -1,27 +1,23 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFCWorldLogicModule.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCWorldLogicModule                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "AFWorldLogicPlugin.h"
 #include "AFCWorldLogicModule.h"
 #include "SDK/Base/AFPlatform.hpp"
@@ -47,4 +43,3 @@ bool AFCWorldLogicModule::AfterInit()
 
     return true;
 }
-
