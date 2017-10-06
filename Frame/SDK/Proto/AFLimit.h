e6d004dbe9fcf6327d5fe671fa796ea29dd0d3de
NickYang <362148418@qq.com>
2017-10-06 18:22:49 +0800

    unify the comment of every files
    Former-commit-id: 24b8ecbd07a834d33169305280d819eebfb253a5 [formerly e82bf0081de7de2e6c4456c95cd5b44582a944d4] [formerly dc2a4a463ff2bfebdbbdb867fba0eca84d3824d5 [formerly 84f02ee14188b57f9860deb306ec6d9e3c7aa28b]]
    Former-commit-id: 5b556b00d41cdbd6c69d477ea2910b127e4dfc62 [formerly a0be38982ee2177b7249af6cbb347df75d9105f1]
    Former-commit-id: d947229946d64bada6451463c1c76f3a70ea94de
    Former-commit-id: b455ece971cf6a4a3cd58c1e8fd298ba9e04d163

--- Frame/SDK/Proto/AFLimit.h
+++ Frame/SDK/Proto/AFLimit.h
@@ -1,29 +1,24 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file  	AFLimit.h                                                *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFLimit                                                  *
-*****************************************************************************/
-#ifndef NF_LIMIT_H
-#define NF_LIMIT_H
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#pragma once
 
 #pragma pack(push,1)
 
@@ -53,6 +48,3 @@
 */
 
 #pragma pack(pop)
-
-#endif
-
