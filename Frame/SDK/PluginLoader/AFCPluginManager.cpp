1c78b80085ef23be32d79b438141a6932a56e0be
NickYang <362148418@qq.com>
2017-10-06 18:22:49 +0800

    unify the comment of every files
    Former-commit-id: ade8eba812b5cb019bc5f31d793de79b78ea4793 [formerly 18148aefbe3b63e36bf63dff0d9d83b0680a75ef] [formerly f91dceeae5e8207fbf7ec11226a8016b87f803c2] [formerly 6ff2f6ae0aafacbae1ece513e23d72e724cd311d [formerly 55e4f0c65cd44f419a937cd9e53df413aa98e863]] [formerly 6ffe5fa8a6921d1bd208cc5184667c084ed02154 [formerly a741f6620e194180b5f6c0c28a20b8bf3b53d2e0] [formerly 082763355b6751257942bd131426d166ca3c833c [formerly 54061a0d08bcb9024a0c83fc83153a2b3593282f]]] [formerly ef20fb09d702039bbbb081b9bf87535cd4ee6b65]
    Former-commit-id: 63c97880f72d0b7f9ee6258f726e865aeb5bc60b
    Former-commit-id: 2967835c89c85c6ea105ca9cc93d72d8271321e9 [formerly 52a5bb42797f8f9f4b9d04bc367c143d6fc8b9c9] [formerly 559711487cc01d556add96f680b81a39af5a75fa [formerly 569b417529c5cc2a71d28a8bb31d440330c7add8]]
    Former-commit-id: 0f541c6c41614c59de3e252cf2da2c543a48b67d [formerly 075dfb31b3a55ca8ff320b2640d6e653a84d672f]
    Former-commit-id: 4bfb458aeee105814dad7677cd31d3b92b4f1d0c
    Former-commit-id: 87c6dc99e92cd2ab60e38a760bbc1be5110a501b

--- Frame/SDK/PluginLoader/AFCPluginManager.cpp
+++ Frame/SDK/PluginLoader/AFCPluginManager.cpp
@@ -1,27 +1,23 @@
-/*****************************************************************************
-// * This source file is part of ArkGameFrame                                *
-// * For the latest info, see https://github.com/ArkGame                     *
-// *                                                                         *
-// * Copyright(c) 2013 - 2017 ArkGame authors.                               *
-// *                                                                         *
-// * Licensed under the Apache License, Version 2.0 (the "License");         *
-// * you may not use this file except in compliance with the License.        *
-// * You may obtain a copy of the License at                                 *
-// *                                                                         *
-// *     http://www.apache.org/licenses/LICENSE-2.0                          *
-// *                                                                         *
-// * Unless required by applicable law or agreed to in writing, software     *
-// * distributed under the License is distributed on an "AS IS" BASIS,       *
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
-// * See the License for the specific language governing permissions and     *
-// * limitations under the License.                                          *
-// *                                                                         *
-// *                                                                         *
-// * @file      AFCPluginManager.cpp                                              *
-// * @author    Ark Game Tech                                                *
-// * @date      2015-12-15                                                   *
-// * @brief     AFCPluginManager                                                  *
-*****************************************************************************/
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2017 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 #include "AFCPluginManager.h"
 #include "RapidXML/rapidxml.hpp"
 #include "RapidXML/rapidxml_iterators.hpp"
@@ -32,28 +28,6 @@
 
 #if ARK_PLATFORM == PLATFORM_WIN
 #pragma comment( lib, "ws2_32.lib" )
-#endif
-
-#if ARK_RUN_MODE == ARK_RUN_MODE_DEBUG
-
-#if ARK_PLATFORM == PLATFORM_WIN
-
-#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == AF_PLATFORM_ANDROID
-#pragma comment( lib, "libtherond.a" )
-#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == AF_PLATFORM_APPLE_IOS
-#pragma comment( lib, "libtherond.a" )
-#endif
-
-#else
-
-#if ARK_PLATFORM == PLATFORM_WIN
-
-#elif ARK_PLATFORM == PLATFORM_UNIX || ARK_PLATFORM == AF_PLATFORM_ANDROID
-#pragma comment( lib, "libtheron.a" )
-#elif ARK_PLATFORM == PLATFORM_APPLE || ARK_PLATFORM == AF_PLATFORM_APPLE_IOS
-#pragma comment( lib, "libtheron.a" )
-#endif
-
 #endif
 
 AFCPluginManager::AFCPluginManager() : AFIPluginManager()
