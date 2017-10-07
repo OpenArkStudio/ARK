f6ab655768939f3be4345eee896df40d09850fe8
flyicegood <flyicegood@163.com>
2017-10-07 20:37:36 +0800

    modify evpp build bat
    Former-commit-id: 1ea3857ac6ebbcc26e220a820abfb4e60b73cc12 [formerly 99bc0b52cd57ea5dec3c750e36bbe73395b2d506] [formerly 9253de9b3b1ebd6bff1f1a2a73f1fd473c6228b3 [formerly 0f377a50d642d654fc8b66b795f11bf3794df499]]
    Former-commit-id: 7a1b59a5459fa569dfc3bd431192f3ea66fd5b57 [formerly fb2c8208582024b46f2cdd88f9722661b8ae4ae1]
    Former-commit-id: 6c68bd813675911680f33e5de26f9afbd5824c36
    Former-commit-id: f0343dbef219d8d31a50e216816f6c399a19d8f5

--- Dep/build_dep.bat
+++ Dep/build_dep.bat
@@ -2,72 +2,13 @@
 
 echo Building dependencies...
 
-if exist lib (rd lib /q /s)
-md lib
-cd lib
-md Debug
-md Release
-cd ../
-REM ######################################################################################################
-echo Building protobuf...
-
-if exist protobuf (rd protobuf /q /s)
-git clone -b 2.7.0 https://github.com/google/protobuf.git
-
-cd protobuf/cmake
-md build
-cd build
-cmake -G "Visual Studio 14 Win64" -Dprotobuf_BUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF ..
-"%VS140COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Debug|x64"
-"%VS140COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Release|x64"
-copy Debug\*.dll ..\..\..\lib\Debug /Y
-copy Debug\*.lib ..\..\..\lib\Debug /Y
-copy Release\*.dll ..\..\..\lib\Release /Y
-copy Release\*.lib ..\..\..\lib\Release /Y
-cd ..\..\..\
-REM ######################################################################################################
-echo Building libevent...
-
-if exist libevent (rd libevent /q /s)
-git clone -b master https://github.com/libevent/libevent.git
-
-cd libevent
-md build
-cd build
-cmake -G "Visual Studio 14 Win64" -DEVENT__DISABLE_OPENSSL=ON ..
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_core_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_core_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_extra_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_extra_static.vcxproj
-copy lib\Debug\*.lib ..\..\lib\Debug /Y
-copy lib\Release\*.lib ..\..\lib\Release /Y
-cd ..\..\
-REM ######################################################################################################
-echo Building glog...
-if exist glog (rd glog /q /s)
-git clone -b v035 https://github.com/google/glog.git
-
-cd glog
-md build
-cd build
-cmake -G "Visual Studio 14 Win64" ..
-"%VS140COMNTOOLS%..\IDE\Devenv" google-glog.sln /build "Debug|x64"
-"%VS140COMNTOOLS%..\IDE\Devenv" google-glog.sln /build "Release|x64"
-copy Debug\*.lib ..\..\lib\Debug /Y
-copy Release\*.lib ..\..\lib\Release /Y
-cd ..\..\
-REM ######################################################################################################
 echo Building evpp...
 
-if exist evpp (rd evpp /q /s)
-git clone -b master https://github.com/ArkGame/evpp.git
 
 cd evpp
 cd vsprojects
-"%VS140COMNTOOLS%..\IDE\Devenv" libevpp.sln /build "Debug|x64" /project evpp_static.vcxproj
-"%VS140COMNTOOLS%..\IDE\Devenv" libevpp.sln /build "Release|x64" /project evpp_static.vcxproj
+"%VS140COMNTOOLS%..\IDE\Devenv" libevpp.sln /build "Debug|x64" /project libevpp.vcxproj
+"%VS140COMNTOOLS%..\IDE\Devenv" libevpp.sln /build "Release|x64" /project libevpp.vcxproj
 cd ..\..\
 REM ####################################################################################################
 REM back to root dir
