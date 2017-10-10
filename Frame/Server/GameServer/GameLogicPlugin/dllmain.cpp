232601af1f6131e3b774d0f12dcc89c0fa2db809
NickYang <362148418@qq.com>
2017-10-10 10:01:01 +0800

    add CMakeLists.txt for Frame/Server
    Former-commit-id: 5b8d34f08a322cb3314205ecba61836a3937603c
    Former-commit-id: 6ee814841165d6fda0bfc4f9b7aeeb5315cd6144
    Former-commit-id: 7c5d31dcf49db3a3e751cc927255ea10dc774cd0

--- Frame/Server/GameServer/GameLogicPlugin/dllmain.cpp
+++ Frame/Server/GameServer/GameLogicPlugin/dllmain.cpp
@@ -27,15 +27,15 @@
 
 #if ARK_RUN_MODE == ARK_RUN_MODE_DEBUG
 
+#pragma comment(lib, "libprotobufd.lib")
 #pragma comment(lib, "AFCore_d.lib")
 #pragma comment(lib, "AFProto_d.lib")
-#pragma comment(lib, "libprotobufd.lib")
 
 #else
 
+#pragma comment(lib, "libprotobuf.lib")
 #pragma comment(lib, "AFCore.lib")
 #pragma comment(lib, "AFProto.lib")
-#pragma comment(lib, "libprotobuf.lib")
 
 #endif
 
