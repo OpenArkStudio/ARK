df9571f7c4622db4c2e48a7a4b9519dbe2d14a9c
NickYang <362148418@qq.com>
2017-10-14 14:49:34 +0800

    unify the interface directory of server
    Former-commit-id: 42d09f82d9482d85954ecbe68a35d355f402e0b6 [formerly 7f7ffd65672cb5a5e6c8ed4fcf2ac87885ce9c26] [formerly b337f36e66e0648ee80ff46cfb7c9cb9270fee00] [formerly de4aaf05372fc9d2265dba77ad43bb27d13dbfb2]
    Former-commit-id: ac304185aad9025a5e43263ad5e31f6b63eb5457
    Former-commit-id: e127318339767a5d6a3ea52721ec3c1b1ab67dc5
    Former-commit-id: f1eff05a484c577aa6544b004dbb482dd79f678f

--- Frame/SDK/Base/AFVector3.hpp
+++ Frame/SDK/Base/AFVector3.hpp
@@ -18,35 +18,13 @@
 *
 */
 
-#ifndef VECTOR_3_HPP
-#define VECTOR_3_HPP
-
-/**
- * @struct  Point3D
- *
- * @brief   3D point structure.
- *
- * @author  Nick Yang
- * @date    2016/11/16
- */
+#pragma once
 
 struct Point3D
 {
-    /** @brief   x coordinate. */
     float x;
-    /** @brief   y coordinate. */
     float y;
-    /** @brief   z coordinate. */
     float z;
-
-    /**
-     * @fn  Point3D()
-     *
-     * @brief   Default constructor.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     */
 
     Point3D()
         : x(.0f)
@@ -55,36 +33,12 @@ struct Point3D
     {
     }
 
-    /**
-     * @fn  Point3D(float f3[])
-     *
-     * @brief   Constructor.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   f3  The float array.
-     */
-
     Point3D(float f3[])
     {
         x = f3[0];
         y = f3[1];
         z = f3[2];
     }
-
-    /**
-     * @fn  Point3D(float a, float b, float c)
-     *
-     * @brief   Constructor.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   a   The float to process.
-     * @param   b   The float to process.
-     * @param   c   The float to process.
-     */
 
     Point3D(float a, float b, float c)
         : x(a),
@@ -93,36 +47,12 @@ struct Point3D
     {
     }
 
-    /**
-     * @fn  Point3D(const Point3D& rht)
-     *
-     * @brief   Constructor.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   rht The rht.
-     */
-
     Point3D(const Point3D& rht)
     {
         x = rht.x;
         y = rht.y;
         z = rht.z;
     }
-
-    /**
-     * @fn  const Point3D& operator=(const Point3D& rht)
-     *
-     * @brief   Assignment operator.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   rht The rht.
-     *
-     * @return  A shallow copy of this object.
-     */
 
     const Point3D& operator=(const Point3D& rht)
     {
@@ -132,84 +62,25 @@ struct Point3D
         return *this;
     }
 
-    /**
-     * @fn  inline bool operator==(const Point3D& rht) const
-     *
-     * @brief   Equality operator.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   rht The rht.
-     *
-     * @return  True if the parameters are considered equivalent.
-     */
-
     inline bool operator==(const Point3D& rht) const
     {
         return IsZeroFloat(Distance(*this, rht));
     }
-
-    /**
-     * @fn  inline bool operator!=(const Point3D& rht) const
-     *
-     * @brief   Inequality operator.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   rht The rht.
-     *
-     * @return  True if the parameters are not considered equivalent.
-     */
 
     inline bool operator!=(const Point3D& rht) const
     {
         return !(*this == rht);
     }
 
-    /**
-     * @fn  float GetLength() const
-     *
-     * @brief   Gets the length.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @return  The length.
-     */
-
     float GetLength() const
     {
         return sqrt((x * x) + (y * y) + (z * z));
     }
 
-    /**
-     * @fn  bool IsZero() const
-     *
-     * @brief   Query if this object is zero.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @return  True if zero, false if not.
-     */
-
     bool IsZero() const
     {
         return (IsZeroFloat(x) && IsZeroFloat(y) && IsZeroFloat(z));
     }
-
-    /**
-     * @fn  std::string ToString() const
-     *
-     * @brief   Convert this object into a string representation.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @return  A std::string that represents this object.
-     */
 
     std::string ToString() const
     {
@@ -217,19 +88,6 @@ struct Point3D
         ss << x << "," << y << "," << z;
         return ss.str();
     }
-
-    /**
-     * @fn  bool FromString(const std::string& strValue)
-     *
-     * @brief   Initializes this object from the given from string.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   strValue    The value.
-     *
-     * @return  True if it succeeds, false if it fails.
-     */
 
     bool FromString(const std::string& strValue)
     {
@@ -272,20 +130,6 @@ struct Point3D
         }
     }
 
-    /**
-     * @fn  static float Distance(const Point3D& a, const Point3D& b)
-     *
-     * @brief   Distances.
-     *
-     * @author  Nick Yang
-     * @date    2016/11/16
-     *
-     * @param   a   The Point3D to process.
-     * @param   b   The Point3D to process.
-     *
-     * @return  A float.
-     */
-
     static float Distance(const Point3D& a, const Point3D& b)
     {
         const float dx = a.x - b.x;
@@ -294,6 +138,3 @@ struct Point3D
         return sqrt(dx * dx + dy * dy + dz * dz);
     }
 };
-
-#endif //!VECTOR_3_HPP
-
