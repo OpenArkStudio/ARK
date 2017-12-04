a2acca9bd94b320f04e6f697dd6e4d81d33f50aa
NickYang <362148418@qq.com>
2017-12-04 11:43:18 +0800

    rename some parameters
    Former-commit-id: 4120a5f1c257d89129dbc2f3fad9100f0f9f9f69 [formerly fd76c0afba0a480bbbc0b625b9e73e39b00b72fe] [formerly fdb95583526985fa20950c8b38cb6be3bfa41fb7 [formerly c80277223acb4937d65ce77435b21c21812fdd8a]]
    Former-commit-id: 7cc3acf192c9b504445619a41040e6bf4420689c [formerly 432187b719a1358f3fb38e08a53d3eb8b88df65b]
    Former-commit-id: 1faf9c506318a7439808a3096c3cfe2c7acf2181
    Former-commit-id: e80b6925fa4a2973929ed1706cda04ff9e2f299b

--- Dep/build_dep.sh
+++ Dep/build_dep.sh
@@ -18,7 +18,7 @@ git clone -b 2.7.0 https://github.com/google/protobuf.git
 cd protobuf/cmake
 mkdir build && cd build
 cmake -G "Unix Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF ..
-make
+make -j 8
 cp -R -f *.so* ../../../lib/Debug
 cp -R -f *.so* ../../../lib/Release
 cp -R -f protoc ../../../lib/Debug
@@ -37,7 +37,7 @@ cd libevent
 chmod -R 755 *
 ./autogen.sh #depend automake & libtool
 ./configure --disable-shared --disable-openssl
-make
+make -j 8
 cp -R -f .libs/*.a ../lib/Debug
 cp -R -f .libs/*.a ../lib/Release
 cd ../
@@ -58,7 +58,7 @@ cd build
 CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 echo $CURDIR
 cmake -G "Unix Makefiles"  -DLIBEVENT_DIR=$CURDIR/../../libevent -DLIBEVENT_LIB_DIR=$CURDIR/../../lib/Release ..
-make
+make -j 8
 cp -R -f lib/*.a ../../lib/Debug
 cp -R -f lib/*.a ../../lib/Release
 cd ../../
