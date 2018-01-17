abf465b1fe59479bb01a8b34ca57c07ab690c6f4
NickYang <362148418@qq.com>
2018-01-17 17:29:45 +0800

    change make_unique
    Former-commit-id: 78cf4a0caee72123819b78396e7fd5a7d5a58fd7 [formerly 16cf5ea9007a2331d74eb65e371f1b1af9c31947] [formerly 9f5b3e7ac8c21ed0349f3c5708eb5cceb1c6b2c4 [formerly b62e7b8958e544ef6049fbf3b2cdbf3889dc078f]]
    Former-commit-id: 3b3fa4b7cd063e80227d6357ebe0285494d809b3 [formerly 312e773067df383882a2994fcf090b4c945c2b7f]
    Former-commit-id: 3b9f1bca8ba34d4bc306dc99435336364e82d05f
    Former-commit-id: 97cf3868efb4f6ee0d12ddf2f0fc07f9d3b26228

--- Frame/SDK/Base/AFMacros.hpp
+++ Frame/SDK/Base/AFMacros.hpp
@@ -224,10 +224,15 @@ bool ARK_TO_STR(std::string& strValue, const T& nValue)
 #define CLEAR_HOUR 5
 
 #if __cplusplus < 201402L
-// note: this implementation does not disable this overload for array types
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args)
+
+namespace std
 {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    // note: this implementation does not disable this overload for array types
+    template<typename T, typename... Args>
+    unique_ptr<T> make_unique(Args&&... args)
+    {
+        return unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
 }
+
 #endif
