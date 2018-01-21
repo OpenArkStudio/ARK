312751769925ddb64f7e9d7fe4ad25de2eac9954
NickYang <362148418@qq.com>
2018-01-21 13:22:19 +0800

    fix net module errors
    Former-commit-id: fa42d75eafa81c0db2ee5f8437a979bdbbb8aa1b [formerly a62728ff95f90a5bddf0ea99755175e03733eec8] [formerly eff73471014fccb0916e7728ee38e00a6e2bf754 [formerly 3e6349bf70af29116511882e227b9f5ec759c336]]
    Former-commit-id: 42fa41c21dfd98b63b3a05b4a900dceac9fbf2a5 [formerly 5897531a25bdad442c08300f88f8099718a8676a]
    Former-commit-id: 40242a85c90ec462156e36d09cb8b6d76905dbcd
    Former-commit-id: 31c865f8c74e79b203f866d21adac6f11586a8a8

--- Bin/Server/Debug/run.bat
+++ Bin/Server/Debug/run.bat
@@ -1,6 +1,8 @@
 cd /d %~dp0
 
 @echo off
+
+echo Copy common dlls...
 copy ..\\..\\..\\Bin\\Comm\\Debug\\PluginLoader_d.exe AFLoginServer\\ /Y
 copy ..\\..\\..\\Bin\\Comm\\Debug\\PluginLoader_d.exe AFMasterServer\\ /Y
 copy ..\\..\\..\\Bin\\Comm\\Debug\\PluginLoader_d.exe AFProxyServer\\ /Y
@@ -12,6 +14,31 @@ copy ..\\..\\..\\Dep\\lib\\Debug\\libproto*d.dll AFMasterServer\\ /Y
 copy ..\\..\\..\\Dep\\lib\\Debug\\libproto*d.dll AFProxyServer\\ /Y
 copy ..\\..\\..\\Dep\\lib\\Debug\\libproto*d.dll AFWorldServer\\ /Y
 copy ..\\..\\..\\Dep\\lib\\Debug\\libproto*d.dll AFGameServer\\ /Y
+
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFproto*d.dll AFLoginServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFproto*d.dll AFMasterServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFproto*d.dll AFProxyServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFproto*d.dll AFWorldServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFproto*d.dll AFGameServer\\ /Y
+
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFKernelPlugin*d.dll AFLoginServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFKernelPlugin*d.dll AFMasterServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFKernelPlugin*d.dll AFProxyServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFKernelPlugin*d.dll AFWorldServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFKernelPlugin*d.dll AFGameServer\\ /Y
+
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFUtilityPlugin*d.dll AFLoginServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFUtilityPlugin*d.dll AFMasterServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFUtilityPlugin*d.dll AFProxyServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFUtilityPlugin*d.dll AFWorldServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFUtilityPlugin*d.dll AFGameServer\\ /Y
+
+echo Copy self dlls
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFLogin*d.dll AFLoginServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFMaster*d.dll AFMasterServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFProxy*d.dll AFProxyServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFWorld*d.dll AFWorldServer\\ /Y
+copy ..\\..\\..\\Bin\\Comm\\Debug\\AFGame*d.dll AFGameServer\\ /Y
 
 cd  AFMasterServer
 
