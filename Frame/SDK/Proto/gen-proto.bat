56b5a9fbad639119ccf7cd3c41bd37915f6a3c8c
NickYang <362148418@qq.com>
2018-02-02 11:47:04 +0800

    update protobuf to version 3.x
    Former-commit-id: e7217a99ad0dbbda7d4dc1ce375b875af082cd8d
    Former-commit-id: 80d747784dca9dfaf6862cf13aa8824c50e14c86

--- Frame/SDK/Proto/gen-proto.bat
+++ Frame/SDK/Proto/gen-proto.bat
@@ -1,6 +1,6 @@
 echo "generate protobuff files"
 
 for /f "delims=" %%i in ('dir /b/a-d/oN *.proto') do "proto-gen/protoc.exe" -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ ./%%~ni.proto
-for /f "delims=" %%i in ('dir /b/a-d/oN *.proto') do "proto-gen/protogen.exe" -i:%%i -o:cs/%%~ni.cs
+for /f "delims=" %%i in ('dir /b/a-d/oN *.proto') do "proto-gen/protoc.exe" -I=./ --csharp_out=./cs/ ./%%~ni.proto
 
 pause
