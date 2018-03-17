4cc56a73534e10f080f725069091b089ce850672
NickYang <362148418@qq.com>
2018-03-17 15:27:25 +0800

    add time-wheel timer add schedule module(unfinished)
    Former-commit-id: e345ecb8048856b676d1db2b9b50f0089398bf48 [formerly ddde628fd5cb995259f6e29e9e071dded3acf37a] [formerly f8bcbb8072f84c5ebd5cf503823066f1194a743f [formerly 446503a8aafff475e41279c0ec928a415e3d65db]]
    Former-commit-id: cfd4d1d196038939c1711051f00feb65cac8bab8 [formerly 17a668fef37ee93eb93054a708b5929d08072a59]
    Former-commit-id: ce21321565c4124222bdf8bb2293efabb839d55b
    Former-commit-id: fd75026c22d01a436dbd25af56b6a19ae7b442ba

---
+++ Frame/SDK/UtilityPlugin/AFCScheduleModule.cpp
@@ -0,0 +1,69 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2018 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#include "AFCScheduleModule.h"
+
+AFCScheduleModule::AFCScheduleModule(AFIPluginManager* p)
+{
+    pPluginManager = p;
+}
+
+bool AFCScheduleModule::Init()
+{
+    return true;
+}
+
+bool AFCScheduleModule::AfterInit()
+{
+    return true;
+}
+
+bool AFCScheduleModule::BeforeShut()
+{
+    return true;
+}
+
+bool AFCScheduleModule::Shut()
+{
+    return true;
+}
+
+void AFCScheduleModule::Update()
+{
+
+}
+
+bool AFCScheduleModule::AddSchedule(const std::string& name, const SCHEDULE_FUNCTOR_PTR cb, const int64_t interval_time, const int count)
+{
+    //TODO:
+    return true;
+}
+
+bool AFCScheduleModule::RemoveSchedule(const std::string& name)
+{
+    //TODO:
+    return true;
+}
+
+bool AFCScheduleModule::ExistSchedule(const std::string& name)
+{
+    //TODO:
+    return true;
+}
