73dae6d9ed67d0f851fc2e68d784660b8678c3ba
NickYang <362148418@qq.com>
2018-03-23 18:09:20 +0800

    add dep build script for vs2017
    Former-commit-id: 779f1f5be3ac194245117759cee56d21d5b79df5 [formerly b9c5489edc398742960c32e8f15325b21ecdaa52] [formerly 2709320d566e3c61caa0a70ed7b0f3418f234f4d [formerly 156132f03f0a3eb50d920d1cf0d65e944ee0868a]]
    Former-commit-id: d1c01dd313c64b30a5be97061c1575997697ba30 [formerly f3c9f7c13f561a235b0fcec96a776ad617b04fff]
    Former-commit-id: 1bf79cac7b67bbf0d3f116391d02a2bc54713e78
    Former-commit-id: 9f1ddb9f523ae09cc65d5e1198ef77c03bcd734b

---
+++ Dep/build_dep_vs2017.bat
@@ -0,0 +1,74 @@
+@echo off
+
+echo Building dependencies...
+
+if exist lib (rd lib /q /s)
+md lib
+cd lib
+md Debug
+md Release
+cd ../
+
+REM If your path is different with blow, please change to your install path
+set %VS150COMNTOOLS%=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\
+echo "%VS150COMNTOOLS%..\IDE\Devenv"
+
+REM ######################################################################################################
+echo Building protobuf...
+
+if exist protobuf (rd protobuf /q /s)
+git clone -b 3.5.x https://github.com/google/protobuf.git
+
+cd protobuf/cmake
+md build
+cd build
+cmake -G "Visual Studio 15 Win64" -Dprotobuf_BUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF ..
+"%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Debug|x64"
+"%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Release|x64"
+copy Debug\*.dll ..\..\..\lib\Debug /Y
+copy Debug\*.lib ..\..\..\lib\Debug /Y
+copy Release\*.dll ..\..\..\lib\Release /Y
+copy Release\*.lib ..\..\..\lib\Release /Y
+
+copy Release\libprotobuf.dll ..\..\..\..\Frame\SDK\Proto\proto-gen /Y
+copy Release\libprotoc.dll ..\..\..\..\Frame\SDK\Proto\proto-gen /Y
+copy Release\protoc.exe ..\..\..\..\Frame\SDK\Proto\proto-gen /Y
+
+cd ..\..\..\
+REM ######################################################################################################
+echo Building libevent...
+
+if exist libevent (rd libevent /q /s)
+git clone -b master https://github.com/libevent/libevent.git
+
+cd libevent
+md build
+cd build
+cmake -G "Visual Studio 15 Win64" -DEVENT__DISABLE_OPENSSL=ON ..
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_core_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_core_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Debug|x64" /project event_extra_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" libevent.sln /build "Release|x64" /project event_extra_static.vcxproj
+copy lib\Debug\*.lib ..\..\lib\Debug /Y
+copy lib\Release\*.lib ..\..\lib\Release /Y
+cd ..\..\
+REM ######################################################################################################
+echo Building evpp...
+if exist evpp (rd evpp /q /s)
+git clone -b master https://github.com/ArkGame/evpp.git
+
+cd evpp
+md build
+cd build
+cmake -G "Visual Studio 15 Win64" -DLIBEVENT_DIR=..\libevent -DLIBEVENT_LIB_DIR=..\lib\Release ..
+"%VS150COMNTOOLS%..\IDE\Devenv" evpp.sln /build "Debug|x64" /project evpp/evpp_static.vcxproj
+"%VS150COMNTOOLS%..\IDE\Devenv" evpp.sln /build "Release|x64" /project evpp/evpp_static.vcxproj
+copy lib\Debug\*.lib ..\..\lib\Debug /Y
+copy lib\Release\*.lib ..\..\lib\Release /Y
+
+cd ..\..\
+REM ####################################################################################################
+REM back to root dir
+cd ..\
