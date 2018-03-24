2aa2ece5c2ba841cbc4391205c0297ddc5cebefd
NickYang <362148418@qq.com>
2018-03-24 16:06:16 +0800

    Add TimerModule
    Former-commit-id: 7b80d72728b0ef2a1487f9b7b015a567016afe9c [formerly f967a11df3c308fc508401ebefedcbb3b6d526f9] [formerly 1687b4104da3fd8cdc8b74f2f1baf1e4b811bcde [formerly 8bb68386d86d332e09698751bef7047946505506]]
    Former-commit-id: 2ab805d4ccbdb3331d37573962a9f296ae2d46db [formerly fea42b74dbddee3c7aa826ee2b9ad510f18740f4]
    Former-commit-id: 9f6c7635513ac5a0e3373f7cf7549d59167b6ba2
    Former-commit-id: 4945097e419963b5d824be490d9b9cc2567b626e

---
+++ Frame/SDK/UtilityPlugin/AFCTimerModule.cpp
@@ -0,0 +1,68 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2018 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#include "AFCTimerModule.h"
+
+bool AFCTimerModule::Init()
+{
+    mxTimerManager = std::make_shared<AFTimerManager>();
+    return true;
+}
+
+bool AFCTimerModule::AfterInit()
+{
+    return true;
+}
+
+bool AFCTimerModule::BeforeShut()
+{
+    mxTimerManager->Shut();
+    return true;
+}
+
+bool AFCTimerModule::Shut()
+{
+    return true;
+}
+
+void AFCTimerModule::Update()
+{
+    mxTimerManager->Update();
+}
+
+bool AFCTimerModule::RemoveTimer(const std::string& name)
+{
+    return mxTimerManager->RemoveTimer(name);
+}
+
+bool AFCTimerModule::RemoveTimer(const std::string& name, const AFGUID& entity_id)
+{
+    return mxTimerManager->RemoveTimer(name, entity_id);
+}
+
+bool AFCTimerModule::AddSingleTimer(const std::string& name, const AFGUID& entity_id, const uint32_t interval_time, const uint32_t count, TIMER_FUNCTOR_PTR cb)
+{
+    return mxTimerManager->AddSingleTimer(name, entity_id, interval_time, count, cb);
+}
+
+bool AFCTimerModule::AddForeverTimer(const std::string& name, const AFGUID& entity_id, const int64_t interval_time, TIMER_FUNCTOR_PTR cb)
+{
+    return mxTimerManager->AddForverTimer(name, entity_id, interval_time, cb);
+}
---
+++ Frame/SDK/UtilityPlugin/AFCTimerModule.h
@@ -0,0 +1,53 @@
+/*
+* This source file is part of ArkGameFrame
+* For the latest info, see https://github.com/ArkGame
+*
+* Copyright (c) 2013-2018 ArkGame authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+#pragma once
+
+#include "SDK/Interface/AFITimerModule.h"
+#include "SDK/Core/Base/AFTimer.hpp"
+
+class AFCTimerModule : public AFITimerModule
+{
+public:
+    AFCTimerModule(AFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+
+    virtual ~AFCTimerModule() {}
+
+    virtual bool Init();
+    virtual bool AfterInit();
+
+    virtual bool BeforeShut();
+    virtual bool Shut();
+
+    virtual void Update();
+
+    virtual bool RemoveTimer(const std::string& name);
+    virtual bool RemoveTimer(const std::string& name, const AFGUID& entity_id);
+
+protected:
+    virtual bool AddSingleTimer(const std::string& name, const AFGUID& entity_id, const uint32_t interval_time, const uint32_t count, TIMER_FUNCTOR_PTR cb);
+    virtual bool AddForeverTimer(const std::string& name, const AFGUID& entity_id, const int64_t interval_time, TIMER_FUNCTOR_PTR cb);
+
+private:
+    std::shared_ptr<AFTimerManager> mxTimerManager;
+};
