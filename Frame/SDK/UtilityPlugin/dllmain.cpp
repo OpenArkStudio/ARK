9b9a34712ce365209e9740f92142a9740ed160f6
NickYang <362148418@qq.com>
2018-03-24 16:06:16 +0800

    Add TimerModule
    Former-commit-id: 13cd7c6b76ae23869c73c7b7d2ad6ff09ff6871c
    Former-commit-id: 6b01e8405da64e9a30624e83c8edb09cf528bbc8

--- Frame/SDK/UtilityPlugin/dllmain.cpp
+++ Frame/SDK/UtilityPlugin/dllmain.cpp
@@ -24,9 +24,9 @@
 #pragma comment( lib, "Dbghelp.lib" )
 
 #if ARK_RUN_MODE == ARK_RUN_MODE_DEBUG
-//#pragma comment(lib, "AFCore_d.lib")
+#pragma comment(lib, "AFCore_d.lib")
 #else
-//#pragma comment(lib, "AFCore.lib")
+#pragma comment(lib, "AFCore.lib")
 #endif
 
 #endif
