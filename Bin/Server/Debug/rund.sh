5ba8fa07b16c162c4f36ccfd61a77ef135572566
NickYang <362148418@qq.com>
2018-03-30 17:58:51 +0800

    Forced to use app_id app_name cfg parameters
    Former-commit-id: 2344d3c51a754eb83bacbd44063cd27c077d30e6 [formerly ceae7c71c8254bc586b166a73abf650e9644cc2b] [formerly 6ccb97064fa5ee0db862b199aa70c9015cf00f98 [formerly dee5a6a61b62d02383985736b8dc6887550de478]]
    Former-commit-id: 3568d65288ca63b9f6b024f33971dc06f5b909d0 [formerly f3329591a096902e38d2253d5c40dbdb6c52e9be]
    Former-commit-id: 045d12aa9754b32125adb3dda93e4c01fcc987b4
    Former-commit-id: 719733441d316c95965e98dc7ecf3d97091a2cad

--- Bin/Server/Debug/rund.sh
+++ Bin/Server/Debug/rund.sh
@@ -1,49 +1,68 @@
-cp -a ../../Comm/Debug/NFPluginLoader_d ./NFMasterServer/NFMasterServer_d
-cp -a ../../Comm/Debug/NFPluginLoader_d ./NFWorldServer/NFWorldServer_d
-cp -a ../../Comm/Debug/NFPluginLoader_d ./NFLoginServer/NFLoginServer_d
-cp -a ../../Comm/Debug/NFPluginLoader_d ./NFGameServer1/NFGameServer_d
-cp -a ../../Comm/Debug/NFPluginLoader_d ./NFProxyServer1/NFProxyServer_d
+echo Copy common dlls...
+cp -a ../../../Bin/Comm/Debug/PluginLoader_d AFLoginServer/
+cp -a ../../../Bin/Comm/Debug/PluginLoader_d AFMasterServer/
+cp -a ../../../Bin/Comm/Debug/PluginLoader_d AFProxyServer/
+cp -a ../../../Bin/Comm/Debug/PluginLoader_d AFWorldServer/
+cp -a ../../../Bin/Comm/Debug/PluginLoader_d AFGameServer/
 
-cp -a ../../Comm/Debug/*.so ./NFMasterServer/
-cp -a ../../Comm/Debug/*.so ./NFWorldServer/
-cp -a ../../Comm/Debug/*.so ./NFLoginServer/
-cp -a ../../Comm/Debug/*.so ./NFGameServer1/
-cp -a ../../Comm/Debug/*.so ./NFProxyServer1/
+cp -a ../../../Dep/lib/Debug/libproto*d.so AFLoginServer/
+cp -a ../../../Dep/lib/Debug/libproto*d.so AFMasterServer/
+cp -a ../../../Dep/lib/Debug/libproto*d.so AFProxyServer/
+cp -a ../../../Dep/lib/Debug/libproto*d.so AFWorldServer/
+cp -a ../../../Dep/lib/Debug/libproto*d.so AFGameServer/
 
+cp -a ../../../Bin/Comm/Debug/AFProto*d.so AFLoginServer/
+cp -a ../../../Bin/Comm/Debug/AFProto*d.so AFMasterServer/
+cp -a ../../../Bin/Comm/Debug/AFProto*d.so AFProxyServer/
+cp -a ../../../Bin/Comm/Debug/AFProto*d.so AFWorldServer/
+cp -a ../../../Bin/Comm/Debug/AFProto*d.so AFGameServer/
 
+cp -a ../../../Bin/Comm/Debug/AFKernelPlugin*d.so AFLoginServer/
+cp -a ../../../Bin/Comm/Debug/AFKernelPlugin*d.so AFMasterServer/
+cp -a ../../../Bin/Comm/Debug/AFKernelPlugin*d.so AFProxyServer/
+cp -a ../../../Bin/Comm/Debug/AFKernelPlugin*d.so AFWorldServer/
+cp -a ../../../Bin/Comm/Debug/AFKernelPlugin*d.so AFGameServer/
+
+cp -a ../../../Bin/Comm/Debug/AFUtilityPlugin*d.so AFLoginServer/
+cp -a ../../../Bin/Comm/Debug/AFUtilityPlugin*d.so AFMasterServer/
+cp -a ../../../Bin/Comm/Debug/AFUtilityPlugin*d.so AFProxyServer/
+cp -a ../../../Bin/Comm/Debug/AFUtilityPlugin*d.so AFWorldServer/
+cp -a ../../../Bin/Comm/Debug/AFUtilityPlugin*d.so AFGameServer/
+
+echo Copy self dlls
+cp -a ../../../Bin/Comm/Debug/AFLogin*d.so AFLoginServer/
+cp -a ../../../Bin/Comm/Debug/AFMaster*d.so AFMasterServer/
+cp -a ../../../Bin/Comm/Debug/AFProxy*d.so AFProxyServer/
+cp -a ../../../Bin/Comm/Debug/AFWorld*d.so AFWorldServer/
+cp -a ../../../Bin/Comm/Debug/AFGame*d.so AFGameServer/
 
 export LC_ALL="C"
 
-cd ./NFMasterServer
-chmod -R 777  NFMasterServer_d
-./NFMasterServer_d -d
-cd ../
+cd AFMasterServer
+echo Starting AFMasterServer...
+./PluginLoader_d -d app_id=3 app_name=MasterServer cfg=Plugin.xml
 
-cd ./NFWorldServer
-chmod -R 777  NFWorldServer_d
-./NFWorldServer_d -d
-cd ../
-
-
-cd ./NFLoginServer
-chmod -R 777  NFLoginServer_d
-./NFLoginServer_d -d
-cd ../
+cd ..
+cd AFWorldServer
+echo Starting AFWorldServer...
+./PluginLoader_d -d app_id=7 app_name=WorldServer cfg=Plugin.xml
 
 
+cd ..
+cd AFLoginServer
+echo Starting AFLoginServer...
+./PluginLoader_d -d app_id=4 app_name=LoginServer cfg=Plugin.xml
 
-cd ./NFGameServer1
-chmod -R 777  NFGameServer_d
-./NFGameServer_d -d
-cd ../
+cd ..
+cd AFGameServer
+echo Starting AFGameServer...
+./PluginLoader_d -d app_id=6 app_name=GameServer cfg=Plugin.xml
 
+cd ..
+cd AFProxyServer
+echo Starting AFProxyServer...
+./PluginLoader_d -d app_id=5 app_name=ProxyServer cfg=Plugin.xml
 
-cd ./NFProxyServer1
-chmod -R 777  NFProxyServer_d
-./NFProxyServer_d -d
-cd ../
+echo All processes are launching...
 
-
-
-
-ps -A|grep NF
+ps ax | grep *Server
