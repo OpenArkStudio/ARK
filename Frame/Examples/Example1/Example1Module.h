be15e886f3fea7efeb14cf49717e4ba07ddc9813
NickYang <362148418@qq.com>
2018-04-09 20:10:44 +0800

    rename samples

--- Frame/Examples/Example1/Example1Module.h
+++ Frame/Examples/Example1/Example1Module.h
@@ -2,7 +2,7 @@
 * This source file is part of ArkGameFrame
 * For the latest info, see https://github.com/ArkGame
 *
-* Copyright (c) 2013-2017 ArkGame authors.
+* Copyright (c) 2013-2018 ArkGame authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -25,10 +25,10 @@
 #include "SDK/Interface/AFITimerModule.h"
 #include "SDK/Interface/AFILogModule.h"
 
-class HelloWorld1 : public AFIModule
+class Example1Module : public AFIModule
 {
 public:
-    HelloWorld1(AFIPluginManager* p)
+    Example1Module(AFIPluginManager* p)
     {
         pPluginManager = p;
     }
