3f80bcdd5d644f588982e3783d4844dabb37ab4d
NickYang <362148418@qq.com>
2018-08-07 14:13:26 +0800

    update build_dep.sh
    Former-commit-id: 8ed9a10ac1bd6504e24f75bcb6bbdccd28f354fe [formerly 87c1ca67f411b6f6aa946ceb095c66afa0f13877] [formerly 0e3902ee7823b54a52982ad0fcf828dd5c4ecd65 [formerly 360dbf21d957c02c546cbe67ceb00c7ada94b53a]]
    Former-commit-id: ebbd6b49069ad830726d884731737e24afe83ca8 [formerly c72bcf6a0dd33017be8fa70bc346c28ab8c7c389]
    Former-commit-id: 06eb00ff896387a172c7c4fdcf224cab51564288

--- Dep/build_dep.sh
+++ Dep/build_dep.sh
@@ -17,13 +17,13 @@ fi
 git clone -b 3.5.x https://github.com/google/protobuf.git --depth 1
 cd protobuf/cmake
 mkdir build && cd build
-cmake -G "Unix Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_BUILD_TYPE=debug -Dprotobuf_BUILD_TESTS=OFF ..
+cmake -G "Unix Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=debug -Dprotobuf_BUILD_TESTS=OFF ..
 make -j 8
 cp -R -f *.so* ../../../lib/Debug
 cp -R -f protoc ../../../lib/Debug
 
 rm -rf ./*
-cmake -G "Unix Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_BUILD_TYPE=release -Dprotobuf_BUILD_TESTS=OFF ..
+cmake -G "Unix Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=release -Dprotobuf_BUILD_TESTS=OFF ..
 make -j 8
 cp -R -f *.so* ../../../lib/Release
 cp -R -f protoc ../../../lib/Release
@@ -44,11 +44,11 @@ git clone -b master https://github.com/ArkGame/brynet.git --depth 1
 cd brynet
 chmod -R 755 *
 mkdir build && cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_BUILD_TYPE=debug ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug ..
 cp -R -f lib/*.a ../../lib/Debug
 
 rm -rf ./*
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_BUILD_TYPE=release ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release ..
 make -j 8
 cp -R -f lib/*.a ../../lib/Release
 cd ../../
