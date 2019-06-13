34366d64e8c189cbb9fcebcf1fee989b7e025729
NickYang <362148418@qq.com>
2019-06-13 11:39:19 +0800

    restructure time wheel timer

--- frame/base/AFTimeWheelTimer.hpp
+++ frame/base/AFTimeWheelTimer.hpp
@@ -28,8 +28,7 @@
 namespace ark
 {
     const uint32_t FREE_TIMER_SIZE = 1000;
-
-    const uint32_t TIMER_PRECISION = 1;     //ms
+    const uint32_t TIMER_PRECISION = 10;    //10 ms per step
     const uint32_t TV_BITS = 8;
     const uint32_t TV_SIZE = 1 << TV_BITS;  //256 slots each time_vec
     const uint32_t TV_MASK = TV_SIZE - 1;
@@ -38,61 +37,45 @@ namespace ark
     class AFTimeWheelData
     {
     public:
-        AFTimeWheelData()
-        {
-
-        }
-
-        int32_t count = 0;      // -1 means forever
-        uint32_t interval = 0;
-        uint64_t expires = 0;
-        uint64_t idx = 0;
-        TIMER_FUNCTOR_PTR callback;
+        int32_t count{ 0 };      // -1 means forever
+        uint32_t interval{ 0 };
+        uint64_t expires{ 0 };
+        uint64_t idx{ 0 };
+        TIMER_FUNCTOR_PTR callback{ nullptr };
 
         //callback data
-        char name[16];
-        AFGUID entity_id = 0;
+        char name[16] { 0 };
+        AFGUID entity_id{ NULL_GUID };
     };
 
     class AFTimeWheelVec
     {
     public:
-        int index;
+        int index{ 0 };
         std::vector<std::list<AFTimeWheelData*>> vec;
     };
 
     class AFTimeWheelManager : public AFSingleton<AFTimeWheelManager>
     {
     public:
-        AFTimeWheelManager()
-        {
-            mnNowSlot = 0;
-            mnLastUpdateTime = 0;
-        }
-
-        ~AFTimeWheelManager()
-        {
-
-        }
-
         void Init(uint64_t now_time)
         {
-            mnNowSlot = 0;
-            mnTimerIdx = 0;
-            mnLastUpdateTime = now_time;
+            now_slot_ = 0;
+            timer_idx_ = 0;
+            last_update_time_ = now_time;
 
             for (int i = 0 ; i < FREE_TIMER_SIZE; ++i)
             {
                 AFTimeWheelData* data = ARK_NEW AFTimeWheelData();
                 memset(data, 0, sizeof(AFTimeWheelData));
-                mxFreeTimers.push_back(data);
+                free_timers_.push_back(data);
             }
 
             for (int i = 0 ; i < TV_NUM ; ++i)
             {
                 AFTimeWheelVec* tv = ARK_NEW AFTimeWheelVec();
                 InitTimerVec(tv, TV_SIZE);
-                mxTVs[i] = tv;
+                tw_vecs_[i] = tv;
             }
         }
 
@@ -103,54 +86,54 @@ namespace ark
 
         void Shut()
         {
-            for (auto iter : mxTimers)
+            for (auto iter : timers_)
             {
                 ARK_DELETE(iter.second);
             }
 
-            for (auto iter : mxFreeTimers)
+            for (auto iter : free_timers_)
             {
                 ARK_DELETE(iter);
             }
 
-            for (auto iter : mxTVs)
+            for (auto iter : tw_vecs_)
             {
                 ARK_DELETE(iter);
             }
 
-            mxTimers.clear();
-            mxFreeTimers.clear();
+            timers_.clear();
+            free_timers_.clear();
         }
 
         uint64_t AddTimer(const std::string& name, const AFGUID& entity_id, int32_t count, uint32_t interval, TIMER_FUNCTOR_PTR callback)
         {
             AFTimeWheelData* timer = GetFreeTimer();
             ARK_STRNCPY(timer->name, name.c_str(), (name.length() > 16) ? 16 : name.length() + 1);
-            timer->idx = ++mnTimerIdx;
-            timer->expires = (interval / TIMER_PRECISION) + mnNowSlot;
+            timer->idx = ++timer_idx_;
+            timer->expires = (interval / TIMER_PRECISION) + now_slot_;
             timer->count = count;
             timer->interval = (interval / TIMER_PRECISION);
             timer->callback = callback;
             timer->entity_id = entity_id;
 
             AddTimerInternal(timer);
-            mxTimers.insert(make_pair(timer->idx, timer));
+            timers_.insert(make_pair(timer->idx, timer));
 
             return timer->idx;
         }
 
         bool RemoveTimer(uint64_t idx)
         {
-            auto iter = mxTimers.find(idx);
-            if (iter == mxTimers.end())
+            auto iter = timers_.find(idx);
+            if (iter == timers_.end())
             {
                 return false;
             }
+
             AFTimeWheelData* timer = iter->second;
-            mxTimers.erase(iter);
+            timers_.erase(iter);
 
             timer->callback = nullptr;
-
             return true;
         }
 
@@ -168,14 +151,14 @@ namespace ark
         AFTimeWheelData* GetFreeTimer()
         {
             AFTimeWheelData* timer = nullptr;
-            if (mxFreeTimers.empty())
+            if (free_timers_.empty())
             {
                 timer = ARK_NEW AFTimeWheelData;
             }
             else
             {
-                timer = mxFreeTimers.front();
-                mxFreeTimers.pop_front();
+                timer = free_timers_.front();
+                free_timers_.pop_front();
             }
 
             memset(timer, 0, sizeof(AFTimeWheelData));
@@ -184,19 +167,19 @@ namespace ark
 
         void AddToFreeTimer(AFTimeWheelData* timer)
         {
-            mxFreeTimers.push_back(timer);
+            free_timers_.push_back(timer);
         }
 
         void UpdateTimer(int64_t now_time)
         {
-            uint64_t passedSlot = (now_time - mnLastUpdateTime) / TIMER_PRECISION;
+            uint64_t passedSlot = (now_time - last_update_time_) / TIMER_PRECISION;
 
             if (passedSlot == 0)
             {
                 return;
             }
 
-            mnLastUpdateTime += passedSlot * TIMER_PRECISION;
+            last_update_time_ += passedSlot * TIMER_PRECISION;
 
             for (uint64_t i = 0; i < passedSlot; ++i)
             {
@@ -207,10 +190,10 @@ namespace ark
         void AddTimerInternal(AFTimeWheelData* timer_data)
         {
             uint64_t expires = timer_data->expires;
-            int64_t idx = expires - mnNowSlot;
+            int64_t idx = expires - now_slot_;
             if (idx <= 0)
             {
-                AFTimeWheelVec* tv = mxTVs[0];
+                AFTimeWheelVec* tv = tw_vecs_[0];
                 tv->vec[tv->index].push_back(timer_data);
                 return;
             }
@@ -223,7 +206,7 @@ namespace ark
                 if ((uint64_t)idx < max_tv_num)
                 {
                     index = ((expires >> (i * TV_BITS)) & TV_MASK);
-                    mxTVs[i]->vec[index].push_back(timer_data);
+                    tw_vecs_[i]->vec[index].push_back(timer_data);
                     return;
                 }
             }
@@ -233,20 +216,20 @@ namespace ark
 
         void UpdateSlotTimer()
         {
-            if (mxTVs[0]->index == 0)
+            if (tw_vecs_[0]->index == 0)
             {
                 int32_t n = 1;
                 do
                 {
-                    CarryTimers(mxTVs[n]);
-                } while (mxTVs[n]->index == 1 && ++n < TV_NUM);
+                    CarryTimers(tw_vecs_[n]);
+                } while (tw_vecs_[n]->index == 1 && ++n < TV_NUM);
             }
 
-            int32_t index = mxTVs[0]->index;
-            while (!mxTVs[0]->vec[index].empty())
+            int32_t index = tw_vecs_[0]->index;
+            while (!tw_vecs_[0]->vec[index].empty())
             {
-                AFTimeWheelData* timer = mxTVs[0]->vec[index].front();
-                mxTVs[0]->vec[index].pop_front();
+                AFTimeWheelData* timer = tw_vecs_[0]->vec[index].front();
+                tw_vecs_[0]->vec[index].pop_front();
 
                 if (timer->callback == nullptr)
                 {
@@ -260,7 +243,7 @@ namespace ark
                 //forever timer
                 if (timer->count < 0 && timer->interval > 0)
                 {
-                    timer->expires = mnNowSlot + timer->interval;
+                    timer->expires = now_slot_ + timer->interval;
                     AddTimerInternal(timer);
                     continue;
                 }
@@ -268,18 +251,18 @@ namespace ark
                 timer->count--;
                 if (timer->count > 0 && timer->interval > 0)
                 {
-                    timer->expires = mnNowSlot + timer->interval;
+                    timer->expires = now_slot_ + timer->interval;
                     AddTimerInternal(timer);
                 }
                 else
                 {
-                    mxTimers.erase(timer->idx);
+                    timers_.erase(timer->idx);
                     AddToFreeTimer(timer);
                 }
             }
 
-            mnNowSlot++;
-            mxTVs[0]->index = (mxTVs[0]->index + 1) & TV_MASK;
+            now_slot_++;
+            tw_vecs_[0]->index = (tw_vecs_[0]->index + 1) & TV_MASK;
         }
 
         void CarryTimers(AFTimeWheelVec* tv)
@@ -296,12 +279,12 @@ namespace ark
         }
 
     private:
-        uint32_t mnNowSlot;
-        AFTimeWheelVec* mxTVs[TV_NUM];
-        uint64_t mnLastUpdateTime;
-        uint64_t mnTimerIdx;
-        std::unordered_map<uint64_t, AFTimeWheelData*> mxTimers;
-        std::list<AFTimeWheelData*> mxFreeTimers;
+        uint32_t now_slot_{0};
+        AFTimeWheelVec* tw_vecs_[TV_NUM];
+        uint64_t last_update_time_{0};
+        uint64_t timer_idx_{0};
+        std::unordered_map<uint64_t, AFTimeWheelData*> timers_;
+        std::list<AFTimeWheelData*> free_timers_;
     };
 
 }
